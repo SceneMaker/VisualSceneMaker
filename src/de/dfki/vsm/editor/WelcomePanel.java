d700844ecdd8a538777c12b4fa3eaffaad8832d4
mfallas <mfallas@INA>
2015-03-17 21:36:13 +0100

    Merge origin/master  WITH Second version of welcome screen
    Conflicts:
    	src/de/dfki/vsm/editor/Editor.java
    	src/de/dfki/vsm/editor/WelcomePanel.java

--- src/de/dfki/vsm/editor/WelcomePanel.java
+++ src/de/dfki/vsm/editor/WelcomePanel.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package de.dfki.vsm.editor;
 
 import de.dfki.vsm.editor.util.Preferences;
@@ -23,6 +28,7 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 
 /**
