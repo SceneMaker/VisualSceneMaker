8a6b9f892e8babb5b10f99e51338dac3b51ee776
Martin Fallas <ignaciofallas@gmail.com>
2015-10-06 16:26:44 +0200

    Sceneflow palette improved. Elements are now available after dragging

--- src/de/dfki/vsm/editor/project/sceneflow/elements/SceneFlowPaletteItem.java
+++ src/de/dfki/vsm/editor/project/sceneflow/elements/SceneFlowPaletteItem.java
@@ -2,9 +2,9 @@ package de.dfki.vsm.editor.project.sceneflow.elements;
 
 import de.dfki.vsm.Preferences;
 import java.awt.Cursor;
-import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
@@ -17,16 +17,19 @@ import java.awt.dnd.DragSourceDragEvent;
 import java.awt.dnd.DragSourceDropEvent;
 import java.awt.dnd.DragSourceEvent;
 import java.awt.dnd.DragSourceListener;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseAdapter;
 
 import java.io.IOException;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.TransferHandler;
 
-public final class SceneFlowPaletteItem extends JButton implements Transferable {
+public final class SceneFlowPaletteItem extends JLabel implements Transferable {
    
     private final String mToolTipText;
     private final ImageIcon mStandardIcon;
@@ -49,48 +52,57 @@ public final class SceneFlowPaletteItem extends JButton implements Transferable 
         mRollOverIcon = rollIcon;
         mDragIcon     = dragIcon;
         mDragableData = data;
-        setContentAreaFilled(false);
+        //setContentAreaFilled(false);
         setFocusable(false);
         setOpaque(false);
-        setBorder(null);
 
         // to be transferred in the Drag
         mTransferHandler = new TransferHandler(text);
         setTransferHandler(mTransferHandler);
-        setVerticalTextPosition(SwingConstants.BOTTOM);
-        setHorizontalTextPosition(SwingConstants.CENTER);
+        setHorizontalAlignment(JLabel.CENTER);
         setToolTipText(mToolTipText + ": " + info);
         setPreferredSize(Preferences.SF_PALETTEITEM_SIZE);
         setMinimumSize(Preferences.SF_PALETTEITEM_SIZE);
         setMaximumSize(Preferences.SF_PALETTEITEM_SIZE);
+        setIcon(mStandardIcon);
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                setIcon(mRollOverIcon);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                setIcon(mStandardIcon);
+            }
+        });
         mDragSourceListener = new DragSourceListener() {
             @Override
             public void dragEnter(DragSourceDragEvent dsde) {
-
-                //System.out.println("drag enter");
+//                System.out.println("drag enter");
             }
 
             @Override
             public void dragOver(DragSourceDragEvent dsde) {
 
-                //System.out.println("drag over");
+//                System.out.println("drag over");
             }
 
             @Override
             public void dropActionChanged(DragSourceDragEvent dsde) {
-                //System.out.println("drag action changed");
+//                System.out.println("drag action changed");
             }
 
             @Override
             public void dragExit(DragSourceEvent dse) {
 
-                //System.out.println("drag exit");
+//                System.out.println("drag exit");
             }
 
             @Override
             public void dragDropEnd(DragSourceDropEvent dsde) {
-
-                //System.out.println("drag drop end");
+//                System.out.println("drag drop end");
             }
         };
         mDragGestureListener = new DragGestureListener() {
@@ -101,7 +113,6 @@ public final class SceneFlowPaletteItem extends JButton implements Transferable 
                 mDragSource.startDrag(event, cur, (SceneFlowPaletteItem) event.getComponent(), mDragSourceListener);
             }
         };
-
         // The Drag will copy the DnDButton rather than moving it
         mDragSource = new DragSource();
         mDragSource.createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_COPY, mDragGestureListener);
@@ -125,13 +136,13 @@ public final class SceneFlowPaletteItem extends JButton implements Transferable 
         return null;
     }
 
-    @Override
-    public final Icon getIcon() {
-        return mStandardIcon;
-    }
+//    @Override
+//    public final Icon getIcon() {
+//        return mStandardIcon;
+//    }
 
-    @Override
-    public final Icon getRolloverIcon() {
-        return mRollOverIcon;
-    }
+//    @Override
+//    public final Icon getRolloverIcon() {
+//        return mRollOverIcon;
+//    }
 }
