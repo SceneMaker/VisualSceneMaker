8d8bfc692b5739e6f5e0572fb0ff75b6ea75febe
mfallas <ignaciofallas@gmail.com>
2016-03-08 13:00:01 +0100

    Fixed CTRL+Z, CTRL+SHIFT+Z did not update toolbar buttons

--- src/de/dfki/vsm/editor/project/sceneflow/SceneFlowToolBar.java
+++ src/de/dfki/vsm/editor/project/sceneflow/SceneFlowToolBar.java
@@ -545,11 +545,9 @@ public class SceneFlowToolBar extends JToolBar implements EventListener {
         //mLogger.message("Refreshing Buttons Of '" + this + "'");
         //*************************************
         //Refresh the buttons SAVE, UNDO and REDO when project have been changed
-        
+        System.out.println("---------------------------------------------------------------------------------------------");
         mSaveProject.setEnabled(mEditorProject.hasChanged());
-
         mUndo.setEnabled(undoAction.isEnabled());
-
         mRedo.setEnabled(redoAction.isEnabled());
         //*************************************
         //mLogger.message("Check execution status '" + this + "'");
