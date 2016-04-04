66b10be0dd79ee8833bd3794c1f10b0d4e1b7267
Patrick Gebhard <gebhard@dfki.de>
2016-04-04 18:25:39 +0200

    addons

--- src/de/dfki/vsm/xtension/stickman/StickmanExecutor.java
+++ src/de/dfki/vsm/xtension/stickman/StickmanExecutor.java
@@ -17,8 +17,10 @@ import de.dfki.vsm.model.config.ConfigFeature;
 import de.dfki.vsm.model.project.PluginConfig;
 import de.dfki.vsm.model.scenescript.ActionFeature;
 import de.dfki.vsm.runtime.activity.AbstractActivity;
+import de.dfki.vsm.runtime.activity.ActionActivity;
 import de.dfki.vsm.runtime.activity.SpeechActivity;
 import de.dfki.vsm.runtime.activity.executor.ActivityExecutor;
+import de.dfki.vsm.runtime.activity.feedback.MarkerFeedback;
 import de.dfki.vsm.runtime.activity.manager.ActivityManager;
 import de.dfki.vsm.runtime.activity.manager.ActivityWorker;
 import de.dfki.vsm.runtime.project.RunTimeProject;
@@ -35,200 +37,237 @@ import java.util.LinkedList;
  */
 public class StickmanExecutor extends ActivityExecutor {
 
-    // The stickman stage window
-    private static StickmanStage mStickmanStage;
-    // The singelton logger instance
-    private final LOGConsoleLogger mLogger = LOGConsoleLogger.getInstance();
-    // The tworld listener
-    private StickmanListener mListener;
-    // The map of processes
-    private final HashMap<String, Process> mProcessMap = new HashMap();
-    // The client thread list
-    private final HashMap<String, StickmanHandler> mClientMap = new HashMap();
-    // The map of activity worker
-    private final HashMap<String, ActivityWorker> mActivityWorkerMap = new HashMap();
-    // The execution id
-    private int sId = 0;
+	// The stickman stage window
+	private static StickmanStage mStickmanStage;
+	// The singelton logger instance
+	private final LOGConsoleLogger mLogger = LOGConsoleLogger.getInstance();
+	// The tworld listener
+	private StickmanListener mListener;
+	// The map of processes
+	private final HashMap<String, Process> mProcessMap = new HashMap();
+	// The client thread list
+	private final HashMap<String, StickmanHandler> mClientMap = new HashMap();
+	// The map of activity worker
+	private final HashMap<String, ActivityWorker> mActivityWorkerMap = new HashMap();
+	// my current activity
+	private AbstractActivity mActivity = null;
 
-    // Construct the executor
-    public StickmanExecutor(final PluginConfig config, final RunTimeProject project) {
-        // Initialize the plugin
-        super(config, project);
-    }
+	// Construct the executor
+	public StickmanExecutor(final PluginConfig config, final RunTimeProject project) {
+		// Initialize the plugin
+		super(config, project);
+	}
 
-    // Accept some socket
-    public void accept(final Socket socket) {
-        // Make new client thread 
-        final StickmanHandler client = new StickmanHandler(socket, this);
-        // Add the client to list
-        // TODO: Get some reasonable name for references here!
-        mClientMap.put(client.getName(), client);
-        // Start the client thread
-        client.start();
-        //
-        mLogger.warning("Accepting " + client.getName() + "");
-    }
+	// Accept some socket
+	public void accept(final Socket socket) {
+		// Make new client thread 
+		final StickmanHandler client = new StickmanHandler(socket, this);
+		// Add the client to list
+		// TODO: Get some reasonable name for references here!
+		mClientMap.put(client.getName(), client);
+		// Start the client thread
+		client.start();
+		//
+		mLogger.warning("Accepting " + client.getName() + "");
+	}
 
-    @Override
-    public final String marker(final long id) {
-        // Stickman style bookmarks
-        return "$" + id;
-    }
+	@Override
+	public final String marker(final long id) {
+		// Stickman style bookmarks
+		return "$" + id;
+	}
 
-    @Override
-    public void execute(AbstractActivity activity, ActivityManager scheduler) {
-        // get action information
-        final String actor = activity.getActor();
-        final String name = activity.getName();
-        final LinkedList<ActionFeature> features = activity.getFeatureList();
+	@Override
+	public void execute(AbstractActivity activity, ActivityManager manager) {
+		// Remember activity
+		mActivity = activity;
+		// get action information
+		final String actor = activity.getActor();
+		final String name = activity.getName();
+		final LinkedList<ActionFeature> features = activity.getFeatureList();
 
-        mLogger.message("Execute Actor " + actor + ", command " + name);
+		mLogger.message("Execute Actor " + actor + ", command " + name);
 
-        Animation stickmanAnimation = new Animation();
-        String animId = "";
+		Animation stickmanAnimation = new Animation();
 
-        if (name.equalsIgnoreCase("Speak")) {
-            if (activity instanceof SpeechActivity) {
-                SpeechActivity sa = (SpeechActivity) activity;
+		if (name.equalsIgnoreCase("Speak")) {
+			if (activity instanceof SpeechActivity) {
+				SpeechActivity sa = (SpeechActivity) activity;
 
-                // create a new word time mark sequence based on the current utterance blocks
-                WordTimeMarkSequence wts = new WordTimeMarkSequence(sa.getTextOnly("$"));
+				// create a new word time mark sequence based on the current utterance blocks
+				WordTimeMarkSequence wts = new WordTimeMarkSequence(sa.getTextOnly("$"));
 
-                LinkedList blocks = sa.getBlocks();
-                for (final Object item : blocks) {
-                    if (!item.toString().contains("$")) {
-                        wts.add(new Word(item.toString()));
-                    } else {
-                        wts.add(new TimeMark(item.toString()));
-                    }
-                }
+				LinkedList blocks = sa.getBlocks();
+				for (final Object item : blocks) {
+					if (!item.toString().contains("$")) {
+						wts.add(new Word(item.toString()));
+					} else {
+						wts.add(new TimeMark(item.toString()));
+					}
+				}
 
-                stickmanAnimation = AnimationLoader.getInstance().loadEventAnimation(mStickmanStage.getStickman(actor), "Speaking", 3000, true);
-                stickmanAnimation.mParameter = wts;
-                animId = stickmanAnimation.mID;
-            }
-        }
+				// TODO add some mouth open animations ...mProject.getScenePlayer().getActivityManager().schedule(100, blocks, new , this);
+				
+				stickmanAnimation = AnimationLoader.getInstance().loadEventAnimation(mStickmanStage.getStickman(actor), "Speaking", 3000, false);
+				stickmanAnimation.mParameter = wts;
 
-        // send command to platform 
-        synchronized (mActivityWorkerMap) {
-            // send command to platform 
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            IOSIndentWriter iosw = new IOSIndentWriter(out);
-            boolean r = XMLUtilities.writeToXMLWriter(stickmanAnimation, iosw);
+				executeAnimationAndWait(stickmanAnimation, stickmanAnimation.mID);
+			}
+		} else {
+			if (activity instanceof ActionActivity) {
+				stickmanAnimation = AnimationLoader.getInstance().loadAnimation(mStickmanStage.getStickman(actor), name, 1000, false);
+				if (stickmanAnimation != null) {
+					executeAnimation(stickmanAnimation);
+				}
+			} else {
+				return;
+			}
+		}
+	}
 
-            try {
-                broadcast(new String(out.toByteArray(), "UTF-8").replace("\n", " "));
-            } catch (UnsupportedEncodingException exc) {
-                mLogger.warning(exc.getMessage());
-            }
+	private  void executeAnimation(Animation stickmanAnimation) {
+		// executeAnimation command to platform 
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		IOSIndentWriter iosw = new IOSIndentWriter(out);
+		boolean r = XMLUtilities.writeToXMLWriter(stickmanAnimation, iosw);
 
-            // organize wait for feedback
-            ActivityWorker cAW = (ActivityWorker) Thread.currentThread();
-            mActivityWorkerMap.put(animId, cAW);
+		try {
+			broadcast(new String(out.toByteArray(), "UTF-8").replace("\n", " "));
+		} catch (UnsupportedEncodingException exc) {
+			mLogger.warning(exc.getMessage());
+		}
+	}
 
-            // wait until we got feedback
-            mLogger.warning("ActivityWorker " + animId + " waiting ....");
+	private  void executeAnimationAndWait(Animation stickmanAnimation, String animId) {
+		// executeAnimation command to platform 
+		synchronized (mActivityWorkerMap) {
+			// executeAnimation command to platform 
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			IOSIndentWriter iosw = new IOSIndentWriter(out);
+			boolean r = XMLUtilities.writeToXMLWriter(stickmanAnimation, iosw);
 
-            while (mActivityWorkerMap.containsValue(cAW)) {
-                try {
-                    mActivityWorkerMap.wait();
-                } catch (InterruptedException exc) {
-                    mLogger.failure(exc.toString());
-                }
-            }
+			try {
+				broadcast(new String(out.toByteArray(), "UTF-8").replace("\n", " "));
+			} catch (UnsupportedEncodingException exc) {
+				mLogger.warning(exc.getMessage());
+			}
 
-            mLogger.warning("ActivityWorker " + animId + "  done ....");
-        }
-        // Return when terminated
-    }
+			// organize wait for feedback
+			ActivityWorker cAW = (ActivityWorker) Thread.currentThread();
+			mActivityWorkerMap.put(animId, cAW);
 
-    @Override
-    public void launch() {
-        // Create the connection
-        mListener = new StickmanListener(8000, this);
-        // Start the connection
-        mListener.start();
+			// wait until we got feedback
+			mLogger.warning("ActivityWorker " + animId + " waiting ....");
 
-        // Get the plugin configuration
-        for (ConfigFeature cf : mConfig.getEntryList()) {
-            mLogger.message("Stickman Plugin Config: " + cf.getKey() + " = " + cf.getValue());
-        }
+			while (mActivityWorkerMap.containsValue(cAW)) {
+				try {
+					mActivityWorkerMap.wait();
+				} catch (InterruptedException exc) {
+					mLogger.failure(exc.toString());
+				}
+			}
 
-        final String host = mConfig.getProperty("smhost");
-        final String port = mConfig.getProperty("smport");
+			mLogger.warning("ActivityWorker " + animId + "  done ....");
+		}
+		// Return when terminated
+	}
 
-        // Start the StickmanStage client application 
-        mLogger.message("Starting StickmanStage Client Application ...");
-        mStickmanStage = StickmanStage.getNetworkInstance(host, Integer.parseInt(port));
+	@Override
+	public void launch() {
+		// Create the connection
+		mListener = new StickmanListener(8000, this);
+		// Start the connection
+		mListener.start();
 
-        // TODO - read config
-        StickmanStage.addStickman("susanne");
-        StickmanStage.addStickman("patrick");
+		// Get the plugin configuration
+		for (ConfigFeature cf : mConfig.getEntryList()) {
+			mLogger.message("Stickman Plugin Config: " + cf.getKey() + " = " + cf.getValue());
+		}
 
-        // wait for stickman stage 
-        while (mClientMap.isEmpty()) {
-            mLogger.message("Waiting for StickmanStage");
-            try {
-                Thread.sleep(1000);
-            } catch (final InterruptedException exc) {
-                mLogger.failure("Error while waiting ...");
-            }
-        }
-    }
+		final String host = mConfig.getProperty("smhost");
+		final String port = mConfig.getProperty("smport");
 
-    @Override
-    public void unload() {
-        // clear the stage
-        StickmanStage.clearStage();
-        // Abort the client threads
-        for (final StickmanHandler client : mClientMap.values()) {
-            client.abort();
-            // Join the client thread
-            try {
-                client.join();
-            } catch (final Exception exc) {
-                mLogger.failure(exc.toString());
-                // Print some information 
-                mLogger.message("Joining client thread");
-            }
-        }
-        // Clear the map of clients 
-        mClientMap.clear();
-        // Abort the server thread
-        try {
-            mListener.abort();
-            // Join the client thread
-            mListener.join();
-            // Print some information 
-            mLogger.message("Joining server thread");
-        } catch (final Exception exc) {
-            mLogger.failure(exc.toString());
-        }
-    }
+		// Start the StickmanStage client application 
+		mLogger.message("Starting StickmanStage Client Application ...");
+		mStickmanStage = StickmanStage.getNetworkInstance(host, Integer.parseInt(port));
 
-    // Handle some message
-    public void handle(final String message, final StickmanHandler client) {
-        mLogger.warning("Handling " + message + "");
+		// TODO - read config
+		StickmanStage.addStickman("susanne");
+		StickmanStage.addStickman("patrick");
 
-        if (message.contains("#ANIM#end#")) {
+		// wait for stickman stage 
+		while (mClientMap.isEmpty()) {
+			mLogger.message("Waiting for StickmanStage");
+			try {
+				Thread.sleep(1000);
+			} catch (final InterruptedException exc) {
+				mLogger.failure("Error while waiting ...");
+			}
+		}
+	}
 
-            int start = message.lastIndexOf("#") + 1;
-            String animId = message.substring(start);
+	@Override
+	public void unload() {
+		// clear the stage
+		StickmanStage.clearStage();
+		// Abort the client threads
+		for (final StickmanHandler client : mClientMap.values()) {
+			client.abort();
+			// Join the client thread
+			try {
+				client.join();
+			} catch (final Exception exc) {
+				mLogger.failure(exc.toString());
+				// Print some information 
+				mLogger.message("Joining client thread");
+			}
+		}
+		// Clear the map of clients 
+		mClientMap.clear();
+		// Abort the server thread
+		try {
+			mListener.abort();
+			// Join the client thread
+			mListener.join();
+			// Print some information 
+			mLogger.message("Joining server thread");
+		} catch (final Exception exc) {
+			mLogger.failure(exc.toString());
+		}
+	}
 
-            synchronized (mActivityWorkerMap) {
-                if (mActivityWorkerMap.containsKey(animId)) {
-                    mActivityWorkerMap.remove(animId);
-                }
-                mActivityWorkerMap.notifyAll();
-            }
-        }
-    }
+	// Handle some message
+	public void handle(final String message, final StickmanHandler client) {
+		mLogger.warning("Handling " + message + "");
+
+		if (message.contains("#ANIM#end#")) {
+
+			int start = message.lastIndexOf("#") + 1;
+			String animId = message.substring(start);
+
+			synchronized (mActivityWorkerMap) {
+				if (mActivityWorkerMap.containsKey(animId)) {
+					mActivityWorkerMap.remove(animId);
+				}
+				mActivityWorkerMap.notifyAll();
+			}
+		}
+
+		if (message.contains("$")) {
+			synchronized (mActivityWorkerMap) {
+				// wake me up ..
+				mActivityWorkerMap.notifyAll();
+				// play the acitiviy
+				mProject.getScenePlayer().getActivityManager().handle(new MarkerFeedback(mActivity, message));
+			}
+		}
+	}
 
 // Broadcast some message
-    private void broadcast(final String message) {
-        for (final StickmanHandler client : mClientMap.values()) {
-            client.send(message);
-        }
-    }
+	private void broadcast(final String message) {
+		for (final StickmanHandler client : mClientMap.values()) {
+			client.send(message);
+		}
+	}
 
 }
