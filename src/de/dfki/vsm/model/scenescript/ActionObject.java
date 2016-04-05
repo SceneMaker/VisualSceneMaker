3e2f31455cddc8cdda3bae726ac22e29369cc01b
Patrick Gebhard <gebhard@dfki.de>
2016-04-05 11:38:54 +0200

    changes in order to parse simple action definiton style correctly

--- src/de/dfki/vsm/model/scenescript/ActionObject.java
+++ src/de/dfki/vsm/model/scenescript/ActionObject.java
@@ -17,234 +17,236 @@ import java.util.LinkedList;
  */
 public final class ActionObject extends UtteranceElement {
 
-    // The Action Arguments
-    private LinkedList<ActionFeature> mFeatureList = new LinkedList<>();
-    // The Name Of The Agent
-    private String mActor;
-    // The Mode Of The Action
-    private String mMode;
-    // The Name Of The Action
-    private String mName;
+	// The Action Arguments
+	private LinkedList<ActionFeature> mFeatureList = new LinkedList<>();
+	// The Name Of The Agent
+	private String mActor;
+	// The Mode Of The Action
+	private String mMode;
+	// The Name Of The Action
+	private String mName;
 
     ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    public ActionObject() {
-        mActor = null;
-        mMode = null;
-        mName = null;
-    }
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	public ActionObject() {
+		mActor = null;
+		mMode = null;
+		mName = null;
+	}
 
     ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    public ActionObject(
-            final int lower, final int upper,
-            final String actor,
-            final String mode,
-            final String name,
-            final LinkedList<ActionFeature> list) {
-        // Initialize The Boundary
-        super(lower, upper);
-        // Initialize The Members
-        mActor = actor;
-        mMode = mode;
-        mName = name;
-        // Initialize Fature List
-        mFeatureList = list;
-    }
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	public ActionObject(
+	  final int lower, final int upper,
+	  final String actor,
+	  final String mode,
+	  final String name,
+	  final LinkedList<ActionFeature> list) {
+		// Initialize The Boundary
+		super(lower, upper);
+		// Initialize The Members
+		mActor = actor;
+		mMode = mode;
+		mName = name;
+		// Initialize Fature List
+		mFeatureList = list;
+	}
 
-    public final String getActor() {
-        return mActor;
-    }
+	public final String getActor() {
+		return mActor;
+	}
 
-    public final String getMode() {
-        return mMode;
-    }
+	public final String getMode() {
+		return mMode;
+	}
 
-    public final String getName() {
-        return mName;
-    }
+	public final String getName() {
+		return mName;
+	}
 
-    public final LinkedList<ActionFeature> getFeatureList() {
-        return mFeatureList;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    public final void setFeatureList(final LinkedList<ActionFeature> list) {
-        mFeatureList = list;
-    }
+	public final LinkedList<ActionFeature> getFeatureList() {
+		return mFeatureList;
+	}
 
     ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    public final LinkedList<ActionFeature> copyFeatureList() {
-
-        // Construct A List Copy
-        final LinkedList<ActionFeature> copy = new LinkedList<>();
-
-        // Copy Each Single Member
-        for (final ActionFeature feature : mFeatureList) {
-            copy.add(feature.getCopy());
-        }
-
-        // Return The Final Clone
-        return copy;
-    }
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	public final void setFeatureList(final LinkedList<ActionFeature> list) {
+		mFeatureList = list;
+	}
 
     ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    public final ActionFeature getValueOf(final String key) {
-        for (final ActionFeature feature : mFeatureList) {
-            if (feature.getKey().equals(key)) {
-                return feature;
-            }
-        }
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	public final LinkedList<ActionFeature> copyFeatureList() {
 
-        return null;
-    }
+		// Construct A List Copy
+		final LinkedList<ActionFeature> copy = new LinkedList<>();
 
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    @Override
-    public final String getText() {
+		// Copy Each Single Member
+		for (final ActionFeature feature : mFeatureList) {
+			copy.add(feature.getCopy());
+		}
 
-        // Append The Identifiers
-        String actor = (mActor == null) ? "" : mActor + " ";
-        String mode = (mMode == null) ? "" : mMode;
-        String name = (mName == null) ? "" : " " + mName;
-        String result = "[" + actor + mode + name;
-        if (!mFeatureList.isEmpty()) {
-            result += " ";
-
-            for (int i = 0; i < mFeatureList.size(); i++) {
-
-                // Append The Member String
-                result += mFeatureList.get(i).getText();
-
-                // Append A Whitespace Now
-                if (i < mFeatureList.size() - 1) {
-                    result += " ";
-                }
-            }
-        }
-
-        return result + "]";
-    }
+		// Return The Final Clone
+		return copy;
+	}
 
     ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    @Override
-    public final String getText(final HashMap<String, String> args) {
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	public final ActionFeature getValueOf(final String key) {
+		for (final ActionFeature feature : mFeatureList) {
+			if (feature.getKey().equals(key)) {
+				return feature;
+			}
+		}
 
-        // Append The Identifiers
-        String actor = (mActor == null) ? "" : mActor + " ";
-        String mode = (mMode == null) ? "" : mMode;
-        String name = (mName == null) ? "" : " " + mName;
-
-        String result = "[" + actor + mode + name;
-
-        if (!mFeatureList.isEmpty()) {
-            result += " ";
-
-            for (int i = 0; i < mFeatureList.size(); i++) {
-
-                // Append The Member String
-                result += mFeatureList.get(i).getText(args);
-
-                // Append A Whitespace Now
-                if (i < mFeatureList.size() - 1) {
-                    result += " ";
-                }
-            }
-        }
-
-        return result + "]";
-    }
+		return null;
+	}
 
     ////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	@Override
+	public final String getText() {
+
+		// Append The Identifiers
+		String actor = (mActor == null || mActor.equalsIgnoreCase("")) ? "" : mActor + " ";
+		String mode = (mMode == null || mMode.equalsIgnoreCase("")) ? "" : mMode;
+		String name = (mName == null) ? "" : (mode.equalsIgnoreCase("")) ? mName : " " + mName;
+		
+		String result = "[" + actor + mode + name;
+		if (!mFeatureList.isEmpty()) {
+			result += " ";
+
+			for (int i = 0; i < mFeatureList.size(); i++) {
+
+				// Append The Member String
+				result += mFeatureList.get(i).getText();
+
+				// Append A Whitespace Now
+				if (i < mFeatureList.size() - 1) {
+					result += " ";
+				}
+			}
+		}
+
+		return result + "]";
+	}
+
     ////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	@Override
+	public final String getText(final HashMap<String, String> args) {
+
+		// Append The Identifiers
+				
+		String actor = (mActor == null || mActor.equalsIgnoreCase("")) ? "" : mActor + " ";
+		String mode = (mMode == null || mMode.equalsIgnoreCase("")) ? "" : mMode;
+		String name = (mName == null) ? "" : (mode.equalsIgnoreCase("")) ? mName : " " + mName;
+		
+		String result = "[" + actor + mode + name;
+
+		if (!mFeatureList.isEmpty()) {
+			result += " ";
+
+			for (int i = 0; i < mFeatureList.size(); i++) {
+
+				// Append The Member String
+				result += mFeatureList.get(i).getText(args);
+
+				// Append A Whitespace Now
+				if (i < mFeatureList.size() - 1) {
+					result += " ";
+				}
+			}
+		}
+
+		return result + "]";
+	}
+
     ////////////////////////////////////////////////////////////////////////////
-    @Override
-    public final void writeXML(final IOSIndentWriter stream) throws XMLWriteError {
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	@Override
+	public final void writeXML(final IOSIndentWriter stream) throws XMLWriteError {
 
-        stream.println("<ActionObject "
-                + "lower=\"" + mLower + "\" "
-                + "upper=\"" + mUpper + "\" "
-                + "actor=\"" + mActor + "\" "
-                + "mode=\"" + mMode + "\" "
-                + "name=\"" + mName + "\" "
-                + ">");
-        stream.push();
+		stream.println("<ActionObject "
+		  + "lower=\"" + mLower + "\" "
+		  + "upper=\"" + mUpper + "\" "
+		  + "actor=\"" + ((mActor == null) ? "" : mActor) + "\" "
+		  + "mode=\"" + ((mMode == null) ? "" : mMode) + "\" "
+		  + "name=\"" + mName + "\" "
+		  + ">");
+		stream.push();
 
-        for (final ActionFeature feature : mFeatureList) {
-            feature.writeXML(stream);
+		for (final ActionFeature feature : mFeatureList) {
+			feature.writeXML(stream);
 
-            if (!feature.equals(mFeatureList.getLast())) {
-                stream.endl();
-            }
-        }
+			if (!feature.equals(mFeatureList.getLast())) {
+				stream.endl();
+			}
+		}
 
-        stream.pop();
-        stream.print("</ActionObject>");
-    }
+		stream.pop();
+		stream.print("</ActionObject>");
+	}
 
     ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-    @Override
-    public final void parseXML(final Element element) throws XMLParseError {
+	////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	@Override
+	public final void parseXML(final Element element) throws XMLParseError {
 
-        // Parse The Boundary
-        mLower = Integer.parseInt(element.getAttribute("lower"));
-        mUpper = Integer.parseInt(element.getAttribute("upper"));
+		// Parse The Boundary
+		mLower = Integer.parseInt(element.getAttribute("lower"));
+		mUpper = Integer.parseInt(element.getAttribute("upper"));
 
-        // Parse The Members
-        mActor = element.getAttribute("actor");
-        mMode = element.getAttribute("mode");
-        mName = element.getAttribute("name");
+		// Parse The Members
+		mActor = element.getAttribute("actor");
+		mMode = element.getAttribute("mode");
+		mName = element.getAttribute("name");
 
-        // Process The Child Nodes
-        XMLParseAction.processChildNodes(element, new XMLParseAction() {
-            @Override
-            public void run(Element element) throws XMLParseError {
+		// Process The Child Nodes
+		XMLParseAction.processChildNodes(element, new XMLParseAction() {
+			@Override
+			public void run(Element element) throws XMLParseError {
 
-                // Get The Child Tag Name
-                final String name = element.getTagName();
+				// Get The Child Tag Name
+				final String name = element.getTagName();
 
-                // Check The Child Tag Name
-                if (name.equals("ActionFeature")) {
+				// Check The Child Tag Name
+				if (name.equals("ActionFeature")) {
 
-                    // Create A New Token Style
-                    final ActionFeature feature = new ActionFeature();
+					// Create A New Token Style
+					final ActionFeature feature = new ActionFeature();
 
-                    // Parse The New Token Style
-                    feature.parseXML(element);
+					// Parse The New Token Style
+					feature.parseXML(element);
 
-                    // Put The New Style To The Map
-                    mFeatureList.add(feature);
-                } else if (name.equals("ActionParam")) {
+					// Put The New Style To The Map
+					mFeatureList.add(feature);
+				} else if (name.equals("ActionParam")) {
 
-                    // Create A New Token Style
-                    final ActionParam feature = new ActionParam();
+					// Create A New Token Style
+					final ActionParam feature = new ActionParam();
 
-                    // Parse The New Token Style
-                    feature.parseXML(element);
+					// Parse The New Token Style
+					feature.parseXML(element);
 
-                    // Put The New Style To The Map
-                    mFeatureList.add(feature);
-                }
-            }
-        });
-    }
+					// Put The New Style To The Map
+					mFeatureList.add(feature);
+				}
+			}
+		});
+	}
 
-    @Override
-    public final ActionObject getCopy() {
-        return new ActionObject(mLower, mUpper, mActor, mMode, mName, copyFeatureList());
-    }
+	@Override
+	public final ActionObject getCopy() {
+		return new ActionObject(mLower, mUpper, mActor, mMode, mName, copyFeatureList());
+	}
 }
