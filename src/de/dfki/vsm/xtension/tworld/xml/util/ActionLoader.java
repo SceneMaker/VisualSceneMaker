935de7cd57cc9cb0833d096c125deab9fdf41c30
EmpaT <EmpaT@134.96.240.205>
2016-05-03 17:57:15 +0200

    addons

--- src/de/dfki/vsm/xtension/tworld/xml/util/ActionLoader.java
+++ src/de/dfki/vsm/xtension/tworld/xml/util/ActionLoader.java
@@ -152,6 +152,34 @@ public class ActionLoader {
 
         return a;
     }
+    
+        public Action loadCharamelAnimation(String cmd, String value1, String value2) {
+        Action a = null;
+
+        String cp = getTWorldCharamelCommandClasspath(cmd);
+       
+        try {
+            Class c = Class.forName(cp);
+            Constructor[] constructors = c.getConstructors();
+            for (Constructor con : constructors) {
+                Class[] params = con.getParameterTypes();
+                if (params.length == 2) {
+                    if (params[0].getSimpleName().equalsIgnoreCase("string") && params[1].getSimpleName().equalsIgnoreCase("string")) {
+                        a = (Action) c.getDeclaredConstructor(params).newInstance(value1, value2);
+                    }
+                }
+
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            mLogger.failure("No Class for TWorld Charamel Action " + cmd + " and value " + value1 + " and avatar id " + value2);
+        }
+
+        if (a != null) {
+            a.setId(getNextID());
+        }
+
+        return a;
+    }
 
     public Action loadCharamelAnimation(String cmd, LinkedList value1, String value2, String value3) {
         Action a = null;
