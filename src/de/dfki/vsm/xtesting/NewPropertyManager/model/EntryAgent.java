51ead400c1a4c8292d4d75e27bbc82b948b4df4e
acepero13 <acepero13@gmail.com>
2016-08-24 10:49:00 +0200

    Fix editor config Deleting new created agent

--- src/de/dfki/vsm/xtesting/NewPropertyManager/model/EntryAgent.java
+++ src/de/dfki/vsm/xtesting/NewPropertyManager/model/EntryAgent.java
@@ -1,14 +1,24 @@
 package de.dfki.vsm.xtesting.NewPropertyManager.model;
 
 import de.dfki.vsm.model.project.AgentConfig;
+import de.dfki.vsm.xtesting.NewPropertyManager.util.AbstractTreeItem;
+import de.dfki.vsm.xtesting.NewPropertyManager.util.ContextTreeItem;
 
 /**
  * Created by alvaro on 6/2/16.
  */
 public class EntryAgent extends AbstractTreeEntry {
     private AgentConfig agentConfig;
+    private AbstractTreeItem contextTreeItem;
     public EntryAgent(String pName){
         name = pName;
+    }
+    public void setContextTreeItem(AbstractTreeItem contextItem){
+        contextTreeItem = contextItem;
+    }
+
+    public AbstractTreeItem getContextTreeItem(){
+        return  contextTreeItem;
     }
 
     public EntryAgent(AgentConfig agent){
