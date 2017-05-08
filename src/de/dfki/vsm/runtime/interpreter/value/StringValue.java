5fad48668490ab2963585bdad5d0ea83d9716160
Gregor Mehlmann <mehlmann@hcm-lab.de>
2017-05-08 10:12:27 +0200

    added contains command for lists

--- src/de/dfki/vsm/runtime/interpreter/value/StringValue.java
+++ src/de/dfki/vsm/runtime/interpreter/value/StringValue.java
@@ -1,43 +1,49 @@
 package de.dfki.vsm.runtime.interpreter.value;
 
-//~--- non-JDK imports --------------------------------------------------------
-
 import de.dfki.vsm.util.TextFormat;
 
 /**
  * @author Gregor Mehlmann
  */
 public class StringValue extends AbstractValue {
+
     private final String mValue;
 
     public StringValue(String value) {
         mValue = value;
     }
 
+    @Override
     public Type getType() {
         return Type.STRING;
     }
 
+    @Override
     public String getAbstractSyntax() {
         return "StringValue(" + mValue + ")";
     }
 
+    @Override
     public String getConcreteSyntax() {
         return "\"" + mValue + "\"";
     }
 
+    @Override
     public String getFormattedSyntax() {
         return TextFormat.formatConstantStringLiteral("\"" + mValue + "\"");
     }
 
+    @Override
     public String getValue() {
         return mValue;
     }
 
+    @Override
     public StringValue getCopy() {
         return new StringValue(mValue);
     }
 
+    @Override
     public boolean equalsValue(AbstractValue value) {
         if (value.getType() == Type.STRING) {
             return mValue.equals(((StringValue) value).mValue);
