0e49db76643091bd4352cda27e74f48de68e8108
alvaro <acepero13@gmail.com>
2017-11-27 13:30:20 +0100

    Refactor match finder

--- src/de/dfki/vsm/editor/util/sceneScript/SceneHighlighter.java
+++ src/de/dfki/vsm/editor/util/sceneScript/SceneHighlighter.java
@@ -1,78 +1,31 @@
 package de.dfki.vsm.editor.util.sceneScript;
 
-import javax.swing.*;
+import de.dfki.vsm.editor.util.sceneScript.beans.HighlightInformation;
+
 import javax.swing.text.BadLocationException;
-import javax.swing.text.DefaultHighlighter;
-import javax.swing.text.Document;
-import javax.swing.text.Highlighter;
 import java.awt.*;
 
-public class SceneHighlighter {
+public class SceneHighlighter extends MatchFinder{
 
     public static final Color SCENE_HIGHLIGHT_COLOR = new Color(56, 216, 120);
-    private final Document document;
-    private final String sceneName;
-    private final String sceneLanguage;
-    private final Highlighter highlighter;
-    private DefaultHighlighter.DefaultHighlightPainter highlightPainter;
 
-    public SceneHighlighter(Document document, String sceneName, String sceneLanguage, Highlighter highlighter){
-        this.document = document;
-        this.sceneName = sceneName;
-        this.sceneLanguage = sceneLanguage;
-        this.highlighter = highlighter;
+    public SceneHighlighter(HighlightInformation documentInformation) {
+        super(documentInformation);
     }
 
-    public int highlightScene() throws BadLocationException {
-        return findAndHightLightScene();
 
-    }
-
-    public void highlightAndScrollToScene(JEditorPane editorPane) throws BadLocationException {
-        int foundPosition = findAndHightLightScene();
-        if(matchNotFound(foundPosition)){
-            return;
+    @Override
+    protected boolean isMatch(String match, int wordStartIndex) {
+        try {
+            return isScene(wordStartIndex) && matchWord(match);
+        } catch (BadLocationException e) {
+           return false;
         }
-        highlight(foundPosition);
-        scrollToScene(editorPane, foundPosition);
+
     }
 
-    private boolean matchNotFound(int foundPosition) {
-        return foundPosition < 0;
-    }
-
-    private void scrollToScene(JEditorPane editorPane, int foundPosition) throws BadLocationException {
-        Rectangle rView = editorPane.modelToView(foundPosition-1);
-        rView.setSize((int)rView.getWidth(),(int)rView.getHeight() + 100);
-        editorPane.scrollRectToVisible(rView);
-    }
-
-
-    private int findAndHightLightScene() throws BadLocationException {
-        for (int index = 0; index + sceneName.length() < document.getLength(); index++) {
-            int foundIndex = find(index);
-            if(foundIndex > 0){
-                return foundIndex;
-            }
-        }
-        return -1;
-    }
-
-    private int find(int index) throws BadLocationException {
-        String match = document.getText(index, sceneName.length());
-        if (sceneName.equals(match) && isScene(index)) {
-            return index;
-        }
-        return -1;
-    }
-
-    private void highlight(int index) throws BadLocationException {
-
-        highlightPainter = new DefaultHighlighter.DefaultHighlightPainter(SCENE_HIGHLIGHT_COLOR);
-        highlighter.addHighlight(index, index + sceneName.length(), highlightPainter);
-    }
 
     private boolean isScene(int index) throws BadLocationException {
-        return document.getText(index + sceneName.length(), 1).equals("\n");
+        return documentInformation.document.getText(index + documentInformation.wordLength, 1).equals("\n");
     }
 }
--- src/de/dfki/vsm/editor/util/sceneScript/ScriptHighlighter.java
+++ src/de/dfki/vsm/editor/util/sceneScript/ScriptHighlighter.java
@@ -9,79 +9,34 @@ public class ScriptHighlighter {
 
     private final LinkedList<Integer> matches;
     private final HighlightInformation documentInformation;
-    private int currentHighlightedItem = -1;
+    private final MatchFinder finder;
 
-    public ScriptHighlighter(HighlightInformation documentInformation) {
-        this.documentInformation = documentInformation;
+    public ScriptHighlighter(HighlightInformation documentInformation, MatchFinder finder) {
         matches = new LinkedList<>();
+        this.documentInformation = documentInformation;
+        this.finder = finder;
     }
 
 
-    public void find() throws BadLocationException {
-        reset();
-        for (int index = 0;
-             index + documentInformation.wordLength < documentInformation.documentLength;
-             index++) {
-            findMatchesInDocument(index);
-        }
 
-    }
-
-    public void reset(){
-        matches.clear();
-        currentHighlightedItem = -1;
-
-    }
-
-    private void findMatchesInDocument(int index) throws BadLocationException {
-        String match = documentInformation.document.getText(index, documentInformation.wordLength);
-        if (documentInformation.wordToFind.equals(match)) {
-            matches.add(index);
-        }
-    }
 
     public void next() throws BadLocationException {
-        if (!hasNext()) return;
-        updateNextPosition();
-        highlightCurrentItem();
+        int wordIndex = finder.next();
+        highlightCurrentItem(wordIndex);
 
     }
 
-    private void highlightCurrentItem() throws BadLocationException {
-        int matchedWordStartIndex = matches.get(currentHighlightedItem);
+    private void highlightCurrentItem(int wordIndex) throws BadLocationException {
+        int matchedWordStartIndex = matches.get(wordIndex);
         DocumentHighlighter documentHighlighter = new DocumentHighlighter(documentInformation);
         documentHighlighter.highlight(matchedWordStartIndex);
     }
 
-    private void updateNextPosition() {
-        if (hasNext()) {
-            currentHighlightedItem++;
-        }
-    }
-
-    private boolean hasNext() {
-        return currentHighlightedItem < matches.size();
-    }
-
-    public void previous() throws BadLocationException {
-        if (!hasPrevious()) return;
-        updatePreviousPosition();
-        highlightCurrentItem();
-    }
-
-    private void updatePreviousPosition() {
-        if (hasPrevious()) {
-            currentHighlightedItem--;
-        }
-    }
-
-    private boolean hasPrevious() {
-        return currentHighlightedItem > 0;
-    }
 
     public void all() throws BadLocationException {
         for (Integer index: matches ) {
-            next();
+            int matchedWordStartIndex = finder.next();
+            highlightCurrentItem(matchedWordStartIndex);
         }
     }
 
