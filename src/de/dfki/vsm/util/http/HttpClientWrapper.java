d4f3801ae7ed78f4b4762b2709329e860ade9337
alvaro <acepero13@gmail.com>
2017-12-24 17:51:03 +0100

    Refactoring: Rearrange code

--- src/de/dfki/vsm/util/http/HttpClientWrapper.java
+++ src/de/dfki/vsm/util/http/HttpClientWrapper.java
@@ -6,8 +6,8 @@ import java.net.URL;
 
 public class HttpClientWrapper {
 
-    private  String url;
     private final String charset;
+    private  String url;
     private HttpURLConnection connection;
     private  HttpResponse response;
 
@@ -34,10 +34,6 @@ public class HttpClientWrapper {
         return this;
     }
 
-    public String getResponse() {
-        return response.getResponse();
-    }
-
     private void tryToOpenConnection()  {
         try {
              openConnection();
@@ -49,11 +45,26 @@ public class HttpClientWrapper {
     private void openConnection() throws IOException {
         connection = (HttpURLConnection) new URL(url).openConnection();
         connection.setRequestProperty("Accept-Charset", charset);
-
     }
 
 
+    private void waitForResponse() throws InterruptedException {
+        while (!response.isDone()) {
+            Thread.sleep(100);
+        }
+    }
+
     public boolean wasRequestSuccessful() {
         return response.wasRequestOk();
+    }
+
+    public HttpClientWrapper read() throws InterruptedException {
+        waitForResponse();
+        connection.disconnect();
+        return this;
+    }
+
+    public String getResponse() {
+        return response.getResponse();
     }
 }
--- tests/de/dfki/vsm/xtension/decad/sender/HTTPClientTest.java
+++ tests/de/dfki/vsm/xtension/decad/sender/HTTPClientTest.java
@@ -6,18 +6,19 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HTTPClientTest {
 
     @Test
     public void testSendToLocalhost() throws InterruptedException, IOException {
         HttpClientWrapper client = new HttpClientWrapper();
-        client.openUrl("http://localhost:5005/animation/list")
-                .get();
-        while (!client.wasRequestSuccessful()) {
-            Thread.sleep(100);
-        }
+        client
+                .openUrl("http://localhost:5005/animation/list")
+                .get()
+                .read();
         assertEquals("[\"Salute\",\"Salsa1\",\"Waving\",\"FullBodyNodYes\",\"FullBodyShakeNo\"]", client.getResponse());
+        assertTrue(client.wasRequestSuccessful());
     }
 }
 
