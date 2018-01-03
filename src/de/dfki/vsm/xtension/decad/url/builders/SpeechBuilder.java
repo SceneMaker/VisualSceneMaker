29743be97a6b59fabba0e4f2f9f20135490270ef
alvaro <acepero13@gmail.com>
2018-01-03 18:07:07 +0100

    Change speak url according to nodejs server

--- src/de/dfki/vsm/xtension/decad/url/builders/SpeechBuilder.java
+++ src/de/dfki/vsm/xtension/decad/url/builders/SpeechBuilder.java
@@ -3,14 +3,17 @@ package de.dfki.vsm.xtension.decad.url.builders;
 public class SpeechBuilder extends AbstractBuilder {
 
     private static final String SPEAK_PATH = "speak";
+    private static final String SPEECH_PATH = "speech";
 
     public SpeechBuilder speak(String textToSpeak) {
+        this.add(SPEECH_PATH);
         this.add(SPEAK_PATH);
         this.add(textToSpeak);
         return this;
     }
 
     public SpeechBuilder speak() {
+        this.add(SPEECH_PATH);
         this.add(SPEAK_PATH);
         return this;
     }
