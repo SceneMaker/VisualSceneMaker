dfa9ce9b1583f6f137a475a9c1bc349997eab95b
alvaro <acepero13@gmail.com>
2018-02-20 21:24:41 +0100

    Add new voice recognition extension

---
+++ src/de/dfki/vsm/xtension/voicerecognition/vad/Ticker.java
@@ -0,0 +1,23 @@
+package de.dfki.vsm.xtension.voicerecognition.vad;
+
+/**
+ * Created by alvaro on 7/7/17.
+ */
+class Ticker {
+    private long silenceStart = System.currentTimeMillis();
+    private long silenceEnd = System.currentTimeMillis();
+
+    void markStartTick() {
+        silenceStart = System.currentTimeMillis();
+    }
+
+    private void markEndTick() {
+        silenceEnd = System.currentTimeMillis();
+    }
+
+    long tick() {
+        markEndTick();
+        long tDelta = silenceEnd - silenceStart;
+        return tDelta;
+    }
+}
