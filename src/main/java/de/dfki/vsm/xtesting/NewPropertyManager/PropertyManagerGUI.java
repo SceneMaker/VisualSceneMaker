4e71913a06f2c7bb7f2a57bbc511daf5a2ec3146
Lenny Händler <lenny@lennyhaendler.com>
2019-03-11 13:42:08 +0100

    Repair property editor

--- src/main/java/de/dfki/vsm/xtesting/NewPropertyManager/PropertyManagerGUI.java
+++ src/main/java/de/dfki/vsm/xtesting/NewPropertyManager/PropertyManagerGUI.java
@@ -63,7 +63,7 @@ public class PropertyManagerGUI {
     }
 
     private void initFX(JFXPanel jfxPanel) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/res/de/dfki/vsm/xtesting/NewPropertyManager/PropertyManager.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/res/PropertyManager.fxml"));
         mController = new PropertyManagerController( mProject);
         fxmlLoader.setController(mController);
         Parent root = null;
