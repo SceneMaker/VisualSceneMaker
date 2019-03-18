b76ed097dfcc2461c96f40402f2f5958a2f860cf
patrick <patrick@134.96.240.229>
2019-03-18 18:03:51 +0100

    ugly hotfix (still testing) for studymaster

--- src/de/dfki/vsm/xtension/studymaster/ReceiverThread.java
+++ src/de/dfki/vsm/xtension/studymaster/ReceiverThread.java
@@ -40,78 +40,87 @@ public class ReceiverThread extends Thread {
 
     @Override
     public void run() {
-        try {
 
 //            mSocket = new MulticastSocket(mPort);
 //            mSocket.setReuseAddress(true);
 //            mSocket.setBroadcast(true);
 //            mSocket.joinGroup(InetAddress.getByName("230.0.0.1"));
-            //Keep a mSocket open to listen to all the UDP trafic that is destined for this port
-            mSocket = new DatagramSocket(null);
-            mSocket.setReuseAddress(true);
-            mSocket.setBroadcast(true);
-            mSocket.bind(new InetSocketAddress(mPort));
+        //Keep a mSocket open to listen to all the UDP trafic that is destined for this port
+        while (mRunning) {
+            try {
+                mSocket = new DatagramSocket(null);
+                mSocket.setReuseAddress(true);
+                mSocket.setBroadcast(true);
+                mSocket.bind(new InetSocketAddress(mPort));
 
-            while (mRunning) {
-                mLogger.message("Ready to receive messages ...");
+                while (mRunning) {
+                    mLogger.message("Ready to receive messages ...");
 
-                //Receive a packet
-                byte[] recvBuf = new byte[15000];
-                DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
-                mSocket.receive(packet);
+                    //Receive a packet
+                    byte[] recvBuf = new byte[15000];
+                    DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+                    mSocket.receive(packet);
 
-                // Read Packet
-                byte[] data = Arrays.copyOf(packet.getData(), packet.getLength());
+                    // Read Packet
+                    byte[] data = Arrays.copyOf(packet.getData(), packet.getLength());
 
-                // Check if this has been received
-                if (data.equals(lastReceivedData)) {
+                    // #ommitted# Check if this has been received
+                    /*  if (data.equals(lastReceivedData)) {
                     mLogger.message("Ommitting message - was received alread");
                     break;
                 }
-                lastReceivedData = data;
+                     */
+                    lastReceivedData = data;
 
-                String message = new String(packet.getData(), "UTF-8").trim();
-                mLogger.message("Message received " + message + " from " + packet.getAddress().getHostAddress());
-                if (message.startsWith(SenderExecutor.sMSG_HEADER)) {
+                    String message = new String(packet.getData(), "UTF-8").trim();
+                    System.out.println("Message in studymaster: " + message);
+                    mLogger.message("Message received " + message + " from " + packet.getAddress().getHostAddress());
+                    if (message.startsWith(SenderExecutor.sMSG_HEADER)) {
 
-                    // parse message
-                    String[] msgParts = message.split(SenderExecutor.sMSG_SEPARATOR);
+                        // parse message
+                        String[] msgParts = message.split(SenderExecutor.sMSG_SEPARATOR);
 
-                    if (msgParts.length > 1) {
-                        String msgHeader = msgParts[0];
-                        String msg = msgParts[1];
-                        String timestamp = "";
-                        String timeinfo = "";
+                        if (msgParts.length > 1) {
+                            String msgHeader = msgParts[0];
+                            String msg = msgParts[1];
+                            String timestamp = "";
+                            String timeinfo = "";
 
-                        if (msg.equalsIgnoreCase("VAR")) {
-                            String var = msgParts[2];
-                            String value = msgParts[3];
+                            if (msg.equalsIgnoreCase("VAR")) {
+                                String var = msgParts[2];
+                                String value = msgParts[3];
 
-                            mExecutor.setSceneFlowVariable(var, value);
-                        } else {
-                            if (msgParts.length > 2) {
-                                timestamp = msgParts[2];
+                                mExecutor.setSceneFlowVariable(var, value);
+                            } else {
+                                if (msgParts.length > 2) {
+                                    timestamp = msgParts[2];
+                                }
+
+                                if (msgParts.length == 4) {
+                                    timeinfo = msgParts[3];
+                                }
+
+                                mExecutor.setSceneFlowVariable(msg);
                             }
-
-                            if (msgParts.length == 4) {
-                                timeinfo = msgParts[3];
-                            }
-
-                            mExecutor.setSceneFlowVariable(msg);
                         }
-                    }
 
-                    //Send a response
+                        //Send a response
 //                    byte[] sendData = "VSMMessage#Received".getBytes();
 //                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
 //                    mSocket.send(sendPacket);
 //
 //                    mLogger.message("Sent confirmation to " + sendPacket.getAddress().getHostAddress());
+                    }
                 }
+            } catch (IOException ex) {
+                mLogger.message("Exception while receiving data ... " + ex.getMessage());
+                System.out.println("Exception in studymaster: " + ex.toString());
             }
-        } catch (IOException ex) {
-            mLogger.message("Exception while receiving data ... " + ex.getMessage());
         }
+        if (!mRunning) {
+            System.out.println("Studymaster Server stoped");
+        }
+
     }
 
     public void stopServer() {
