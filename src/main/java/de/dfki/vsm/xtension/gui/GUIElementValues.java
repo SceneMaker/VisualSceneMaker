fec669edf6aa6740a248d223908761a0d13644e6
Patrick <gebhard@dfki.de>
2019-08-22 17:34:35 +0200

    Refactored xtension/buttongui package. New name is gui due to extended functionality: display of interactive buttons and textfields and images. Different ui schemas are supported. format for buttion, image and textfield can be found in GUIExecutor.

--- src/main/java/de/dfki/vsm/xtension/gui/GUIElementValues.java
+++ src/main/java/de/dfki/vsm/xtension/gui/GUIElementValues.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.dfki.vsm.xtension.button;
+package de.dfki.vsm.xtension.gui;
 
 /**
  *
