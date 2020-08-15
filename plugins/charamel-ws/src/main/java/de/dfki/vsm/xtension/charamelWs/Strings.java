b447db230597e5dc79755fbdb0756657dc575a8c
Lenny Händler <lenny@lennyhaendler.com>
2020-08-15 12:39:15 +0200

    Replacing static strings
    * actor id no longer hard coded
    * generate UUIDs instead of having them statically

--- plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Strings.java
+++ plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Strings.java
@@ -1,5 +1,7 @@
 package de.dfki.vsm.xtension.charamelWs;
 
+import java.util.UUID;
+
 class Strings {
     static String testMsg = "{\n" +
             "  \"type\": \"animation\",\n" +
@@ -100,42 +102,13 @@ class Strings {
             "  ]\n" +
             "}";
 
-    static String speakCommand(String voice, String text) {
+    static String speakCommand(String voice, String text, String actorId) {
         return ("{\n" +
                 "  \"type\": \"animation\",\n" +
                 "  \"name\": \"\",\n" +
-                "  \"uuid\": \"58a11974-146c-4e3a-ab47-b180922cdec9\",\n" +
+                "  \"uuid\": \"" + UUID.randomUUID() + "\",\n" +
                 "  \"timeline\": [\n" +
-                new SpeakCommand(text, voice).toJsonCommand() +
-                "    {\n" +
-                "      \"type\": \"timeline-element\",\n" +
-                "      \"subtype\": \"motion\",\n" +
-                "      \"uuid\": \"d291dacd-d53d-4968-b425-cfaa9e44a6cc\",\n" +
-                "      \"name\": \"greet01.glb\",\n" +
-                "      \"track\": \"motions1_uuid\",\n" +
-                "      \"timestamp\": 0,\n" +
-                "      \"duration\": 2400,\n" +
-                "      \"data\": {\n" +
-                "        \"attack\": 500,\n" +
-                "        \"decay\": 500,\n" +
-                "        \"speed\": 1,\n" +
-                "        \"path\": \"humanoid/interaction/greet/greet01.glb\"\n" +
-                "      }\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"type\": \"timeline-element\",\n" +
-                "      \"subtype\": \"morph\",\n" +
-                "      \"uuid\": \"10536d63-fc88-4183-aed5-735ea9170da2\",\n" +
-                "      \"name\": \"emot_happy\",\n" +
-                "      \"track\": \"morph_uuid\",\n" +
-                "      \"timestamp\": 600,\n" +
-                "      \"duration\": 2000,\n" +
-                "      \"data\": {\n" +
-                "        \"morph\": \"uuid_emot_happy\",\n" +
-                "        \"attack\": 500,\n" +
-                "        \"decay\": 500\n" +
-                "      }\n" +
-                "    }\n" +
+                new SpeakCommand(text, voice, actorId).toJsonCommand() +
                 "  ]\n" +
                 "}");
     }
