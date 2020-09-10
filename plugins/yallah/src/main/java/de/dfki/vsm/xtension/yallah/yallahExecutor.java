2511b16fd16483953718dd3bbcc172a9944b758a
Fabrizio Nunnari <fab.nunnari@gmail.com>
2020-09-10 12:04:45 +0200

    Renamed YallahExecutor to proper cameling

--- plugins/yallah/src/main/java/de/dfki/vsm/xtension/yallah/yallahExecutor.java
+++ plugins/yallah/src/main/java/de/dfki/vsm/xtension/yallah/yallahExecutor.java
@@ -5,23 +5,29 @@ import de.dfki.vsm.runtime.activity.AbstractActivity;
 import de.dfki.vsm.runtime.activity.executor.ActivityExecutor;
 import de.dfki.vsm.runtime.project.RunTimeProject;
 
-public class yallahExecutor extends ActivityExecutor {
-    public yallahExecutor(PluginConfig config, RunTimeProject project) {
+public class YallahExecutor extends ActivityExecutor {
+
+    public YallahExecutor(PluginConfig config, RunTimeProject project)
+    {
         super(config, project);
     }
 
     @Override
-    public String marker(long id) {
-        return null;
+    public String marker(long id)
+    {
+        return "$"+id;
     }
 
     @Override
     public void execute(AbstractActivity activity) {
-
+        System.out.println("YALLAH Agent " + activity.getActor() + " said: " + activity.getText());
     }
 
     @Override
     public void launch() {
+
+        //
+        // TODO --  launch either the desktop app or the web page
 
     }
 
