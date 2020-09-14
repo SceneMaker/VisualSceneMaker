a2c6ec7d0f1077222d97142d1f36c8492654e0a0
Fabrizio Nunnari <fab.nunnari@gmail.com>
2020-09-14 13:03:37 +0200

    Simplified plugin search code

--- src/main/java/de/dfki/vsm/xtesting/NewPropertyManager/util/ExtensionsFromJar.java
+++ src/main/java/de/dfki/vsm/xtesting/NewPropertyManager/util/ExtensionsFromJar.java
@@ -1,7 +1,10 @@
 package de.dfki.vsm.xtesting.NewPropertyManager.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -15,106 +18,105 @@ import java.util.jar.JarFile;
  */
 public class ExtensionsFromJar {
 
-    private  ArrayList<String> mScenePlayersShortNames = new ArrayList<>();
-    private  ArrayList<String> mScenePlayersLongNames = new ArrayList<>();
-    private  String packageName;
-    private  boolean nonSelectedFirst;
-    public ExtensionsFromJar(String packName, boolean pAddNonSelectedFirst){
+    private ArrayList<String> mScenePlayersShortNames = new ArrayList<>();
+    private ArrayList<String> mScenePlayersLongNames = new ArrayList<>();
+    private String packageName;
+    private boolean nonSelectedFirst;
+
+    public ExtensionsFromJar(String packName, boolean pAddNonSelectedFirst) {
         packageName = packName;
         nonSelectedFirst = pAddNonSelectedFirst;
     }
 
-    public ExtensionsFromJar(String packName){
-        packageName = packName;
-        nonSelectedFirst = true;
+    public ExtensionsFromJar(String packName) {
+        this(packName, true);
     }
-    public ArrayList getActivitiesShortNames(){
+
+    public ArrayList getActivitiesShortNames() {
         return mScenePlayersShortNames;
     }
 
-    public ArrayList getActivitiesLongName(){
+    public ArrayList getActivitiesLongName() {
         return mScenePlayersLongNames;
     }
 
-    public  void loadClass() {
-        try{
+    public void loadClass() {
+        try {
             if (mScenePlayersShortNames.size() <= 0) {
-                addNoSelectedAtFirst();
+
+                if (nonSelectedFirst) {
+                    mScenePlayersShortNames.add("Non selected");
+                }
+
                 getClassNamesFromPackage(packageName);
             }
-        } catch (IOException e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public  void getClassNamesFromPackage(String packageName) throws IOException {
+    public void getClassNamesFromPackage(String packageName) throws IOException, URISyntaxException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        URL packageURL;
-        ArrayList<String> names = new ArrayList<>();
         packageName = packageName.replace(".", "/");
-        packageURL = classLoader.getResource(packageName);
-        if (packageURL.getProtocol().equals("jar")) {
-            packageIsJar(packageURL);
+        // URL packageURL = classLoader.getResource(packageName);
+
+        Enumeration<URL> resources = classLoader.getResources(packageName);
+
+        while (resources.hasMoreElements()) {
+            URL packageURL = resources.nextElement();
+
+
+            // https://stackoverflow.com/a/7461653/2010713
+            if (packageURL.getProtocol().equals("jar")) {
+                parseJar(packageURL);
+            }
+
         }
     }
 
-    private void addNoSelectedAtFirst(){
-        if(nonSelectedFirst){
-            mScenePlayersShortNames.add("Non selected");
-        }
-    }
-
-    private void packageIsJar(URL packageURL) throws UnsupportedEncodingException {
-        String jarFileName;
+    /**
+     * Scan all classes (.class files) in the given Jar and add them to the
+     * class-level list of class names if they are subclasses of Activity Executor.
+     *
+     * @param packageURL
+     */
+    private void parseJar(URL packageURL) {
         // build jar file name, then loop through zipped entries
-        jarFileName = URLDecoder.decode(packageURL.getFile(), StandardCharsets.UTF_8);
+        String jarFileName = URLDecoder.decode(packageURL.getFile(), StandardCharsets.UTF_8);
         jarFileName = jarFileName.substring(5, jarFileName.indexOf("!"));
         System.out.println(">" + jarFileName);
-        parseJar(jarFileName);
-    }
 
-    private  void parseJar(String jarFileName){
-        JarFile jf;
-        Enumeration<JarEntry> jarEntries = null;
         try {
-            jf = new JarFile(jarFileName);
-            jarEntries = jf.entries();
-            ExtractExtensions( jarEntries);
-        } catch (IOException e) {
+            JarFile jf = new JarFile(jarFileName);
+            Enumeration<JarEntry> jarEntries = jf.entries();
+
+            // Look for Activity Executors in all entries
+            while (jarEntries.hasMoreElements()) {
+                String entryName = jarEntries.nextElement().getName();
+                try {
+                    if (isActivityExecutor(entryName)) {
+                        addActivityExecutor(entryName);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Exception checking entry "+entryName+ " --> " + e);
+                }
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private  void ExtractExtensions( Enumeration<JarEntry> jarEntries) {
-        while (jarEntries.hasMoreElements())
-        {
-            tryToAddActivityExecutor(jarEntries);
-        }
-    }
-
-    private void tryToAddActivityExecutor(Enumeration<JarEntry> jarEntries) {
-        String entryName;
-        entryName = jarEntries.nextElement().getName();
-        try {
-            if (isActivityExecutor(entryName)){
-                addActivityExecutor(entryName);
-            }
-        } catch (ClassNotFoundException e) {
-
-        }
-
-    }
 
     private boolean isActivityExecutor(String entryName) throws ClassNotFoundException {
         entryName = entryName.replace("/", ".");
         boolean belongsToPackage = (packageName.length() == 0 || (entryName.startsWith(packageName) && entryName.length() > packageName.length() + 5));
-        if(belongsToPackage) {
+        if (belongsToPackage) {
             String fullClassName = entryName.replace("/", ".");
             String className = fullClassName.substring(0, entryName.lastIndexOf('.'));
             Class classEntry = Class.forName(className);
             Class superClass = classEntry.getSuperclass();
-            return  (superClass != null && (superClass.getSimpleName().equals("ActivityExecutor")));
+            return (superClass != null && (superClass.getSimpleName().equals("ActivityExecutor")));
         }
         return false;
 
