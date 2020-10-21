8f2ecfe3bc6d3cdbde91df1e0d25a05b71973c81
Fabrizio Nunnari <fab.nunnari@gmail.com>
2020-10-21 20:34:03 +0200

    Simplified plugin structure to support only 1 client. Properly composing JSON messages for the YALLAH client.

--- plugins/yallah/src/main/java/de/dfki/vsm/xtension/yallah/YallahExecutor.java
+++ plugins/yallah/src/main/java/de/dfki/vsm/xtension/yallah/YallahExecutor.java
@@ -3,6 +3,8 @@ package de.dfki.vsm.xtension.yallah;
 import de.dfki.vsm.extensionAPI.ExportableProperties;
 import de.dfki.vsm.extensionAPI.ProjectProperty;
 import de.dfki.vsm.extensionAPI.value.ProjectValueProperty;
+import de.dfki.vsm.model.scenescript.ActionFeature;
+import de.dfki.vsm.runtime.activity.SpeechActivity;
 import de.dfki.vsm.runtime.activity.scheduler.ActivityWorker;
 import io.javalin.Javalin;
 
@@ -16,10 +18,7 @@ import io.javalin.websocket.WsContext;
 import io.javalin.websocket.WsMessageContext;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class YallahExecutor extends ActivityExecutor implements ExportableProperties {
 
@@ -29,8 +28,8 @@ public class YallahExecutor extends ActivityExecutor implements ExportableProper
     // The websocket server reading messages coming from the YALLAH visualizer
     private Javalin mWebSocketServer;
 
-    // ??
-    private final ArrayList<WsConnectContext> websockets = new ArrayList<>();
+    // The connection instance associated to the connected client
+    private WsConnectContext mSocketConnectCtx = null;
 
 
     // The map of activity worker
@@ -42,51 +41,34 @@ public class YallahExecutor extends ActivityExecutor implements ExportableProper
         super(config, project);
     }
 
+    private final static String MARKER = "$" ;
+
     @Override
     public String marker(long id)
     {
-        return "$"+id;
+        return MARKER+id;
     }
 
-    @Override
-    public void execute(AbstractActivity activity) {
-        mLogger.message("YALLAH Agent '" + activity.getActor() + "' said: " + activity.getText());
-
-        mLogger.message("Activity type: " + activity.getType() + ", features: " + activity.getFeatures());
-
-        //activity.getSubstitutions().forEach((k, v) -> subs += k + ", " + v + "; ");
-        if(activity.getSubstitutions() != null) {
-            String subs = "";
-            for (Map.Entry<String, String> e : activity.getSubstitutions().entrySet()) {
-                String k = e.getKey();
-                String v = e.getValue();
-                subs += subs += k + ", " + v + "; ";
-            }
-            mLogger.message(subs);
-        } else {
-            mLogger.message("No substitutions");
-        }
-
-    }
 
     @Override
     public void launch() {
         mLogger.message("YALLAH plugin launching...");
 
         //
-        // Setup the WebSocket server to get messages from the YALLAH application.
+        // Setup the WebSocket server to get messages from a YALLAH application.
         final int port = Integer.parseInt(Objects.requireNonNull(mConfig.getProperty("port")));
-        mLogger.message("Launching the WebSocket server on port " + port + "...");
+        mLogger.message("Starting the WebSocket server on port " + port + "...");
 
         //mWebSocketServer = Javalin.create(config -> config.enforceSsl = true).start(port);
         mWebSocketServer = Javalin.create().start(port);
-        // mWebSocketServer.ws("/ws", ws -> {
+
+        // This sequence sets up what the websocket server must do when events occur
         mWebSocketServer.ws("/", ws -> {
 
             // Client Connection
             ws.onConnect(ctx -> {
                 mLogger.message("Getting connection request.");
-                this.addWs(ctx);
+                mSocketConnectCtx = ctx;
             });
 
             // Client Message
@@ -94,7 +76,7 @@ public class YallahExecutor extends ActivityExecutor implements ExportableProper
 
             // Client disconnection
             ws.onClose(ctx -> {
-                this.removeWs(ctx);
+                mSocketConnectCtx = null;
 
                 mLogger.message("Socket closed");
 
@@ -125,6 +107,9 @@ public class YallahExecutor extends ActivityExecutor implements ExportableProper
             case WebPage:
                 // TODO -- run an internal web server that provides the webpages and open them in a browser.
                 mLogger.failure("Launch mode " + lm.toString() + "not supported yet. YALLAH not launched.");
+                break;
+            default:
+                mLogger.warning("Unrecognized launch method " + lm.toString() );
         }
 
 
@@ -141,30 +126,101 @@ public class YallahExecutor extends ActivityExecutor implements ExportableProper
 
         // 2. Shutdown the WebSocket server
         mWebSocketServer.stop();
-
-        websockets.clear();
+        mSocketConnectCtx = null ;
 
         mLogger.message("YALLAH unloaded.");
     }
 
 
-    /** Invoked every time the WebSocket server receives a message */
-    private void handleMessage(@NotNull WsMessageContext wsMessageContext) {
-        mLogger.message("YALLAH: received websocket message: "+wsMessageContext.message());
+    @Override
+    public void execute(AbstractActivity activity) {
 
-        // Answer
-        for (WsContext ws : websockets) {
-            ws.send("Hi, too!");
+        mLogger.message("YALLAH Agent '" + activity.getActor() + "' said: " + activity.getText());
+        mLogger.message("Activity name: " + activity.getName() + ", type: " + activity.getType() + ", features: " + activity.getFeatures());
+
+        //activity.getSubstitutions().forEach((k, v) -> subs += k + ", " + v + "; ");
+        if(activity.getSubstitutions() != null) {
+            String subs = "";
+            for (Map.Entry<String, String> e : activity.getSubstitutions().entrySet()) {
+                String k = e.getKey();
+                String v = e.getValue();
+                subs += k + ", " + v + "; ";
+            }
+            mLogger.message(subs);
+        } else {
+            mLogger.message("No substitutions");
+        }
+
+        if (activity instanceof SpeechActivity) {
+            //
+            // SPEECH activity
+            SpeechActivity sa = (SpeechActivity) activity;
+
+            String punct = sa.getPunct() ;
+            String text_only = sa.getTextOnly(MARKER) ;
+            LinkedList<String> time_marks = sa.getTimeMarks(MARKER);
+
+            mLogger.message("This is a Speech Activity. text only: '" + text_only + "'; punct: '" + punct + "'"
+                    + "There are " + time_marks.size() + " time marks") ;
+            time_marks.forEach(mLogger::message);
+
+            //
+            // Send the sentence to the client
+            if(mSocketConnectCtx != null) {
+                String text = activity.getText() ;
+
+                String json_string = "{\n"
+                        + "\"type\": \"text\",\n"
+                        + "\"text\": \"" + text + "\"\n"
+                        + "}" ;
+
+                mSocketConnectCtx.send(json_string);
+            }
+        } else {
+            //
+            // It is an [ACTION (with parameters)]
+
+            String cmd = activity.getName() ;
+            final LinkedList<ActionFeature> features = activity.getFeatures();
+
+            String json_string = "{\n"
+                    + "\"type\": \"command\",\n"
+                    + "\"command\": \"" + cmd + "\",\n"
+                    + "\"parameters\": \"" ;
+
+            // The Action "features" are actually the parameters.
+            for (int i = 0, featuresSize = features.size(); i < featuresSize; i++) {
+                ActionFeature af = features.get(i);
+
+                json_string += af.getKey() + "=" + af.getVal() ;
+                // Add a comma only if it is not the last element
+                if (i < featuresSize-1) json_string += "," ;
+            }
+
+            json_string += "\"\n"
+                    + "}\n" ;
+
+            if(mSocketConnectCtx != null) {
+                mSocketConnectCtx.send(json_string);
+            }
+
         }
 
     }
 
-    private synchronized void removeWs(@NotNull WsCloseContext ctx) {
-        this.websockets.remove(ctx);
-    }
 
-    private synchronized void addWs(@NotNull WsConnectContext ws) {
-        this.websockets.add(ws);
+    /** Invoked every time the WebSocket server receives a message */
+    private void handleMessage(@NotNull WsMessageContext wsMessageContext) {
+        String message = wsMessageContext.message();
+
+        mLogger.message("YALLAH: Tell VSM activity scheduler to handle action represented by time marker >" + message + "<");
+        if (mProject.getRunTimePlayer().getActivityScheduler().hasMarker(message)) {
+            mProject.getRunTimePlayer().getActivityScheduler().handle(message);
+        } else {
+            mLogger.failure("Marker has already been processed!");
+        }
+
+
     }
 
 
