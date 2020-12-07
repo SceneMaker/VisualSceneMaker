03e75a4fb1fc4645e6e11a1106feb2754d347ff3
gebhard <patrick.gebhard@dfki.de>
2020-12-07 16:13:42 +0100

    added some more parameters for happy and smile commands

--- plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Commands/EmotionCommand.java
+++ plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Commands/EmotionCommand.java
@@ -3,21 +3,32 @@ package de.dfki.vsm.xtension.charamelWs.Commands;
 public class EmotionCommand implements Broadcastable {
     private final String emotionname;
     private final float intensity;
+    private final int attack;
+    private final int hold;
+
 
     public EmotionCommand(String name, float intensity) {
         this.emotionname = name;
         this.intensity = intensity;
+        this.hold = 1000;
+        this.attack = 200;
+    }
+
+    public EmotionCommand(String name, float intensity, int attack, int hold) {
+        this.emotionname = name;
+        this.intensity = intensity;
+        this.attack = attack;
+        this.hold = hold;
     }
 
     public String toJson() {
         return "    {\n" +
                 "      \"type\": \"emotion\",\n" +
                 "      \"name\": \"" + emotionname + "\",\n" +
-                "      \"attack\": 200,\n" +
-                "      \"hold\": 1000,\n" +
+                "      \"attack\": " + attack + ",\n" +
+                "      \"hold\": " + hold + ",\n" +
                 "      \"decay\": 200,\n" +
                 "      \"value\": " + intensity + "\n" +
                 "    }\n";
-
     }
 }
