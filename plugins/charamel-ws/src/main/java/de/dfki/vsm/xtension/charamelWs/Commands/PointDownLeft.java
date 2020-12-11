6e7fc353cd8ecdc28f879bfbb3e80f33acd74945
gebhard <patrick.gebhard@dfki.de>
2020-12-11 07:44:34 +0100

    added two move aminations

---
+++ plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Commands/PointDownLeft.java
@@ -0,0 +1,8 @@
+package de.dfki.vsm.xtension.charamelWs.Commands;
+
+public class PointDownLeft extends ActionCommand {
+
+    public PointDownLeft() {
+        super("humanoid/presentation/point/", "point_handl_08.glb");
+    }
+}
