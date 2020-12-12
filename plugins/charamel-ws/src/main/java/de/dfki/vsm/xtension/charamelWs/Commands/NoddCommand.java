8aa931ef79ca2ba291d0adb3be53eb9107e8cfef
gebhard <patrick.gebhard@dfki.de>
2020-12-12 19:17:00 +0100

    addons - changed animations

--- plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Commands/NoddCommand.java
+++
@@ -1,8 +0,0 @@
-package de.dfki.vsm.xtension.charamelWs.Commands;
-
-public class NoddCommand extends ActionCommand {
-
-    public NoddCommand() {
-        super("humanoid/Talk/introduce/", "Introduce01.glb");
-    }
-}
--- plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Commands/PointDownRight.java
+++ plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Commands/PointDownRight.java
@@ -3,6 +3,6 @@ package de.dfki.vsm.xtension.charamelWs.Commands;
 public class PointDownRight extends ActionCommand {
 
     public PointDownRight() {
-        super("humanoid/presentation/point/", "point_handr_04.glb");
+        super("humanoid/presentation/point/", "screen_show_24.glb");
     }
 }
