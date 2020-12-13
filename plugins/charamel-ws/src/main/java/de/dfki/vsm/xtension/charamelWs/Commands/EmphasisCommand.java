4de4f2bef13ad3a9baff92a48316909283711a2a
gebhard <patrick.gebhard@dfki.de>
2020-12-13 18:22:06 +0100

    bug fixes - changed animation path

---
+++ plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/Commands/EmphasisCommand.java
@@ -0,0 +1,8 @@
+package de.dfki.vsm.xtension.charamelWs.Commands;
+
+public class EmphasisCommand extends ActionCommand {
+
+    public EmphasisCommand() {
+        super("humanoid/talk/emphasize/", "emphasize_openhands_01.glb");
+    }
+}
