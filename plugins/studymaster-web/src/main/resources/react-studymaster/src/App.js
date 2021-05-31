07f6a71ffd2907604df06bc2f7a912c5e8079c6b
Chirag Bhuvaneshwara <chiragbhuvaneshwar@gmail.com>
2021-05-31 21:40:01 +0200

    updated app to correct submit, cancel functions
    On submit or cancel, GUI becomes blank. If one of the fields is not filles, the GUI complains. Also involves refactoring and a somewhat better organization of the react code.

--- plugins/studymaster-web/src/main/resources/react-studymaster/src/App.js
+++ plugins/studymaster-web/src/main/resources/react-studymaster/src/App.js
@@ -3,168 +3,202 @@ import './App.css';
 
 
 function App() {
-  const [text, settext] = useState("");
-  const [ws, setws] = useState();
-  const [state, setState] = useState();
-  const inputValue = new Map();
-  useEffect(() => {
-    let ws = new WebSocket('ws://' + document.location.host + '/ws');
+    const [connectionStatusText, setConnectionStatusText] = useState("");
+    const [webSocket, setWebSocket] = useState();
+    const [formContents, setFormContents] = useState();
+    const inputValue = new Map();
+    useEffect(() => {
+        let ws = new WebSocket('ws://' + document.location.host + '/ws');
 
-    settext('Connecting...');
-    ws.onopen = function () {
-      settext('Connected!');
-    };
-    ws.onclose = function () {
-      settext('Lost connection');
-    };
-    ws.onmessage = function (msg) {
-      console.log(msg.data);
-      const parts = msg.data.split('#');
-      const command = parts[1];
-      if (command === "REQUEST") {
-        setState({
-          action: command,
-          variable: parts[3].split(';'),
-          options: parts[4].split(';'),
-          type: parts[5].split(';'),
-          timestamp: parts[2]
-        })
-      }
-      settext(msg.data);
-    };
-    setws(ws);
-  }, []);
+        setConnectionStatusText('Connecting...');
+        ws.onopen = function () {
+            setConnectionStatusText('Connected!');
+        };
+        ws.onclose = function () {
+            setConnectionStatusText('Lost connection');
+        };
+        ws.onmessage = function (msg) {
+            console.log(msg.data);
+            const parts = msg.data.split('#');
+            const command = parts[1];
+            if (command === "REQUEST") {
+                setFormContents({
+                    action: command,
+                    variable: parts[3].split(';'),
+                    options: parts[4].split(';'),
+                    type: parts[5].split(';'),
+                    timestamp: parts[2]
+                })
+            }
+            setConnectionStatusText(msg.data);
+        };
+        setWebSocket(ws);
+    }, []);
 
-  function sendmsg(e) {
-    e.preventDefault();
-    ws.send(e.target.in.value)
-  }
 
-  function sendSubmit(e){
-      ws.send(`VSMMessage#VAR#request_result#SUBMIT`);
-      sendVar(e);
-  }
+    function sendSubmit(e) {
+        webSocket.send(`VSMMessage#VAR#request_result#SUBMIT`);
+        let allFieldSet = extractAndSendUserInput(e);
 
-  function sendCancel(e){
-      ws.send(`VSMMessage#VAR#request_result#CANCEL`);
-      sendVar(e);
-  }
-
-  // If submit or cancel button is being pushed:
-  // Send all variables with their selected/ written value.
-  function sendVar(e) {
-    e.preventDefault();
-    var i;
-    for (i = 0; i < state.variable.length; i++) {
-      var variable = state.variable[i];
-      if (state.type[i] === "radio") {
-
-        var j;
-        var values = state.options[i].split(',');
-        for (j = 0; j < values.length; j++) {
-          var value = values[j];
-          var radioButtonValue = document.getElementById(value);
-          if (radioButtonValue.checked) {
-            ws.send(`VSMMessage#VAR#${variable}#${value}`);
-          }
-        }
-      } else if (state.type[i] === "text") {
-        if (inputValue.has(variable)){
-          ws.send(`VSMMessage#VAR#${variable}#${inputValue.get(variable)}`);
-        }
-      } else if (state.type[i] === "checkbox") {
-        if (document.getElementById(variable).checked) {
-          ws.send(`VSMMessage#VAR#${variable}#true`);
+        if (allFieldSet) {
+            setFormContents({
+                action: "SUCCESSFULSEND",
+                timestamp: formContents.timestamp,
+            })
         } else {
-          ws.send(`VSMMessage#VAR#${variable}#false`);
+            window.location.reload();
         }
-      }
     }
-  }
 
-  function sendGo() {
-    ws.send("VSMMessage#Go")
-  }
+    function sendCancel() {
+        webSocket.send(`VSMMessage#VAR#request_result#CANCEL`);
 
-  function inputWithType(i) {
-    var variable = state.variable[i];
-    if (state.type[i] === "text") {
-      return (
-        <>
-        <p>
-            <label> {state.variable[i]} </label>
-            <input type="text" name={variable} placeholder={state.options[i]} id={variable} onChange={e => inputValue.set(variable, e.target.value)}/>
-        </p>
-        </>
-      )
-    } else if (state.type[i] === "radio") {
-      var values = state.options[i].split(',');
-      return (
-        <>
-        <p>
-        <label> {state.variable[i]} </label>
-        {values.map((option) =>
+        // Resetting form to empty
+        setFormContents(undefined);
+    }
+
+    // If submit or cancel button is being pushed:
+    // Send all variables with the selected/ written value from user input.
+    function extractAndSendUserInput(e) {
+        e.preventDefault();
+        let allFieldsSet = true;
+        let i;
+        for (i = 0; i < formContents.variable.length; i++) {
+            let variable = formContents.variable[i];
+            if (formContents.type[i] === "radio") {
+
+                let j;
+                let values = formContents.options[i].split(',');
+                let radio_checked = false;
+                for (j = 0; j < values.length; j++) {
+                    let value = values[j];
+                    let radioButtonValue = document.getElementById(value);
+                    if (radioButtonValue.checked) {
+                        webSocket.send(`VSMMessage#VAR#${variable}#${value}`);
+                        radio_checked = true;
+                    }
+                }
+                if (!radio_checked) {
+                    alert("Please ensure to fill in radio input for " + formContents.variable[i]);
+                    allFieldsSet = false;
+                }
+            } else if (formContents.type[i] === "text") {
+                if (inputValue.has(variable)) {
+                    webSocket.send(`VSMMessage#VAR#${variable}#${inputValue.get(variable)}`);
+                } else {
+                    alert("Please ensure to fill in text input for " + formContents.variable[i]);
+                    allFieldsSet = false;
+                }
+            } else if (formContents.type[i] === "number") {
+                if (inputValue.has(variable)) {
+                    webSocket.send(`VSMMessage#VAR#${variable}#${inputValue.get(variable)}`);
+                } else {
+                    alert("Please ensure to fill in number input for " + formContents.variable[i]);
+                    allFieldsSet = false;
+                }
+            } else if (formContents.type[i] === "checkbox") {
+                if (document.getElementById(variable).checked) {
+                    webSocket.send(`VSMMessage#VAR#${variable}#true`);
+                } else {
+                    webSocket.send(`VSMMessage#VAR#${variable}#false`);
+                }
+            }
+        }
+
+        return allFieldsSet
+    }
+
+
+    function generateInputFieldWithType(i) {
+        let variable = formContents.variable[i];
+        if (formContents.type[i] === "text") {
+            return (
+                <>
+                    <p>
+                        <label> {formContents.variable[i]} </label>
+                        <input type="text" name={variable} placeholder={formContents.options[i]} id={variable}
+                               onChange={e => inputValue.set(variable, e.target.value)}/>
+                    </p>
+                </>
+            )
+        } else if (formContents.type[i] === "number") {
+            return (
+                <>
+                    <p>
+                        <label> {formContents.variable[i]} </label>
+                        <input type="number" name={variable} placeholder={formContents.options[i]} id={variable}
+                               onChange={e => inputValue.set(variable, e.target.value)}/>
+                    </p>
+                </>
+            )
+        } else if (formContents.type[i] === "radio") {
+            let values = formContents.options[i].split(',');
+            return (
+                <>
+                    <p>
+                        <label> {formContents.variable[i]} </label>
+                        {values.map((option) =>
+                            <>
+                                <input type="radio" id={option} name={variable} value={option}/>
+                                <label> {option} </label>
+                            </>
+                        )}
+                    </p>
+                </>
+            )
+        } else if (formContents.type[i] === "checkbox") {
+            return (
+                <>
+                    <p>
+                        <label> {formContents.variable[i]} </label>
+                        <input type="checkbox" id={variable} name={variable} value={formContents.options[i]}/>
+                        <label> {formContents.options[i]} </label>
+                    </p>
+                </>
+            )
+        }
+    }
+
+
+    function generateFields() {
+        let returnValue = [];
+
+        let i;
+        for (i = 0; i < formContents.variable.length; i++) {
+            let inputBoxes = generateInputFieldWithType(i);
+            returnValue.push(inputBoxes);
+        }
+        return (
             <>
-            <input type="radio" id={option} name={variable} value={option}/>
-            <label> {option} </label>
+                {returnValue}
             </>
-        )}
-        </p>
-        </>
-      )
-    } else if (state.type[i] === "checkbox") {
-      return (
-          <>
-          <p>
-            <label> {state.variable[i]} </label>
-            <input type="checkbox" id={variable} name={variable} value={state.options[i]}/>
-            <label> {state.options[i]} </label>
-          </p>
-          </>
-      )
+        )
     }
-  }
 
 
-  function returnAllFieldset() {
-    var returnValue = [];
-
-    var i;
-    for (i = 0; i < state.variable.length; i++) {
-      var inputBoxes = inputWithType(i);
-      returnValue.push(inputBoxes);
+    function getSuccessMsg() {
+        return (
+            <h2>
+                Successfully posted!
+            </h2>
+        )
     }
-    return(
-    <>
-        {returnValue}
-    </>
-    )
-  }
 
 
-  return (
-    <div className="App">
-    <header className="App-header">
-    {/*
-       <img src={logo} className="App-logo" alt="logo" />
-      <button onClick={sendGo}> Go </button>
-      <form onSubmit={sendmsg}>
-        <input id = 'in' />
-        <button type='submit'> send </button>
-        {text}
-      </form>
-    */}
-    <form>
-        <fieldset>
-            {(state && (state.action === "REQUEST")) && returnAllFieldset()}
-        </fieldset>
-        <button onClick={sendSubmit}> submit </button>
-        <button onClick={sendCancel}> cancel </button>
-    </form>
-    </header>
-    </div>
-  )
-  ;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <form>
+                    <fieldset>
+                        {(formContents && (formContents.action === "REQUEST")) && generateFields()}
+                        {(formContents && (formContents.action === "SUCCESSFULSEND")) && getSuccessMsg()}
+                    </fieldset>
+                    <button onClick={sendSubmit}> submit</button>
+                    <button onClick={sendCancel}> cancel</button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
