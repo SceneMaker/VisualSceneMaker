b6b5b3caf9d1b6efc358f7c10a8a2fb899ceec74
Chirag <chiragbhuvaneshwar@gmail.com>
2022-01-11 14:46:47 +0100

    Updated with better var names and init css

--- plugins/studymaster-web/src/main/resources/react-studymaster/src/components/infoLogUnit.js
+++ plugins/studymaster-web/src/main/resources/react-studymaster/src/components/infoLogUnit.js
@@ -2,20 +2,14 @@ import {Row} from "react-bootstrap";
 import React from "react";
 
 function InfoLogUnit(props) {
-    return(
-    <Row style={{
-        minHeight: "400 px",
-    }}>
-        <div className="" style={{
-            minHeight: "400 px",
-            backgroundColor: "black"
-        }}>
-            <div className="h-100 d-inline-block">
-                {(props.infoLogContents && (props.infoLogContents.action === "INFORM")) &&
-                <h1 style={{color: "white"}}>{props.infoLogContents.message}</h1>}
+    return (
+            <div className="" style={{
+            }}>
+                <div className="">
+                    {(props.infoLogContents && (props.infoLogContents.action === "INFORM")) &&
+                    <h1 style={{color: "white"}}>{props.infoLogContents.message}</h1>}
+                </div>
             </div>
-        </div>
-    </Row>
     )
 }
 
--- plugins/studymaster-web/src/main/resources/react-studymaster/src/components/inputSheetUnit.js
+++ plugins/studymaster-web/src/main/resources/react-studymaster/src/components/inputSheetUnit.js
@@ -1,47 +1,49 @@
 import GenerateFields from "./inputAllFields";
-import {Col, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import React from "react";
 
 function InputSheetUnit(props) {
 
     return (
-        <Row>
-            <form style={{
-                height: "200 px",
-                backgroundColor: "silver"
-            }}>
-                <fieldset>
-                    {(props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "REQUEST"))}
-                    <GenerateFields updateUserSubmittedInfo={props.updateUserSubmittedInfo}
-                                    inputSheetFieldDetails={props.inputSheetFieldDetails}/>
+        <Container>
+            <Row>
+                <form style={{
+                    // height: "200 px",
+                    // backgroundColor: "silver"
+                }}>
+                    <fieldset>
+                        {(props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "REQUEST"))}
+                        <GenerateFields updateUserSubmittedInfo={props.updateUserSubmittedInfo}
+                                        inputSheetFieldDetails={props.inputSheetFieldDetails}/>
 
+                        {
+                            (props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "SUCCESSFULSEND")) &&
+                            <div>
+                                <h2 style={{color: "green"}}>Successfully posted!</h2>
+                            </div>
+                        }
+                    </fieldset>
                     {
-                        (props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "SUCCESSFULSEND")) &&
+                        (props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "REQUEST")) &&
+                        <Row style={{
+                            margin: "10px"
+                        }}>
+                            <Row xs={2}>
+                                <Button as={Col} variant="primary" onClick={props.sendSubmit}> Submit</Button>
+                                <Button as={Col} variant="secondary" onClick={props.sendCancel}> Cancel</Button>
+                            </Row>
+                        </Row>
+                    }
+                    {
+                        !(props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "REQUEST")) &&
                         <div>
-                            <h2 style={{color: "green"}}>Successfully posted!</h2>
+                            <p style={{color: "white"}}>No active requests.</p>
                         </div>
                     }
-                </fieldset>
-                {
-                    (props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "REQUEST")) &&
-                    <Row style={{
-                        margin: "10px"
-                    }}>
-                        <Row xs={2}>
-                            <Button as={Col} variant="primary" onClick={props.sendSubmit}> Submit</Button>
-                            <Button as={Col} variant="secondary" onClick={props.sendCancel}> Cancel</Button>
-                        </Row>
-                    </Row>
-                }
-                {
-                    !(props.inputSheetFieldDetails && (props.inputSheetFieldDetails.action === "REQUEST")) &&
-                    <div>
-                        <p style={{color: "white"}}>No active requests.</p>
-                    </div>
-                }
-            </form>
-        </Row>
+                </form>
+            </Row>
+        </Container>
     )
 }
 
