5bbe6d2c68adde231d9a48aa8c6ee22860335ac5
Chirag <chiragbhuvaneshwar@gmail.com>
2022-01-14 15:39:11 +0100

    updated studymaster UI with better design and better code struct

--- plugins/studymaster-web/src/main/resources/react-studymaster/src/components/infoLogUnit.js
+++ plugins/studymaster-web/src/main/resources/react-studymaster/src/components/infoLogUnit.js
@@ -1,15 +1,75 @@
-import {Row} from "react-bootstrap";
-import React from "react";
+import {Button, Row} from "react-bootstrap";
+import React, {useState} from "react";
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import {Card, Collapse} from "@material-ui/core";
 
 function InfoLogUnit(props) {
+    const [open, setOpen] = useState(false);
+    const [vsmVars, setVsmVars] = useState({
+        name: "Karla",
+        gender: "f",
+    });
+
+    // var renderVsmVars = vsmVars.map((d) => <li key={d.name}>{d.name}</li>);
+
     return (
-            <div className="" style={{
-            }}>
-                <div className="">
-                    {(props.infoLogContents && (props.infoLogContents.action === "INFORM")) &&
-                    <h1 style={{color: "white"}}>{props.infoLogContents.message}</h1>}
-                </div>
+        <div className="sidebar-divider">
+            <div className="button-area">
+                <Button
+                    onClick={() => {
+                        props.setCollapseDevToolComp(!props.collapseDevToolComp);
+                        setOpen(!open);
+                    }}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                >
+                    {
+                        (open) &&
+                        <div>
+                            <DeveloperModeIcon/> Developer Tools
+                        </div>
+                    }
+                    {
+                        (!open) &&
+                        < DeveloperModeIcon/>
+                    }
+                </Button>
             </div>
+
+            <div className="variable-list">
+                <Row>
+                    <div className="logbox" style={{minHeight: '35vh', color: 'white'}}>
+                        <Collapse in={open} dimension="width">
+                            <div id="example-collapse-text">
+                                VSM Variable List (Work in Progress)
+                                <div>
+                                    {/*{renderVsmVars}*/}
+                                    {
+                                        Object.keys(vsmVars).map((key, index) => (
+                                            <p key={index}> {key} : {vsmVars[key]}</p>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </Collapse>
+                    </div>
+                </Row>
+            </div>
+
+            <div className="log">
+                <Row>
+                    <div className="logbox" style={{minHeight: '35vh', color: 'white'}}>
+                        <Collapse in={open} dimension="width">
+                            <div id="example-collapse-text">
+                                Log history (Work in Progress)
+                            </div>
+                        </Collapse>
+                    </div>
+                </Row>
+            </div>
+
+        </div>
+
     )
 }
 
