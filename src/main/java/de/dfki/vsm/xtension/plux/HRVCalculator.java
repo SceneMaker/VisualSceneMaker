aafe7777e25eb05e06d5dd999ec8f683b8ec5a1e
Daksitha <daksitha.withanage@gmail.com>
2022-02-17 18:26:03 +0100

    imported old code

---
+++ src/main/java/de/dfki/vsm/xtension/plux/HRVCalculator.java
@@ -0,0 +1,16 @@
+package de.dfki.vsm.xtension.plux;
+
+public class HRVCalculator extends Thread{
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    @Override
+    public synchronized void start() {
+        super.start();
+    }
+
+    
+}
