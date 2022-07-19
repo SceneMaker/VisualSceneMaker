200256e67fa061be6968e6946e87fc43bcc6db7e
Manuel <stomack87@gmail.com>
2022-07-20 01:08:53 +0200

    second barebone

--- plugins/mithos/src/main/java/de/dfki/vsm/xtension/mithos/MithosExecutor.java
+++ plugins/mithos/src/main/java/de/dfki/vsm/xtension/mithos/MithosExecutor.java
@@ -4,13 +4,14 @@ import de.dfki.vsm.model.project.PluginConfig;
 import de.dfki.vsm.runtime.activity.AbstractActivity;
 import de.dfki.vsm.runtime.activity.executor.ActivityExecutor;
 import de.dfki.vsm.runtime.project.RunTimeProject;
+import de.dfki.vsm.util.log.LOGConsoleLogger;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import java.util.Properties;
 
 /**
  * This plugin uses a kafka server to control an agent-environment and to receive processed userdata.
- * kafka implementation based on: https://dzone.com/articles/writing-a-kafka-consumer-in-java
+ * kafka producer implementation based on mithos semvox producer demo
  *
  * @author Manuel Anglet
  */
@@ -21,24 +22,13 @@ public class MithosExecutor extends ActivityExecutor {
     private final String topic;
     KafkaProducer<String, String> producer;
     MithosHandler handler;
-
+    private final LOGConsoleLogger mLogger = LOGConsoleLogger.getInstance();
 
     public MithosExecutor(PluginConfig config, RunTimeProject project) {
         super(config, project);
-
+        System.out.println("Mithos Kafka starting");
         server = mConfig.getProperty("server");
         topic = mConfig.getProperty("topic");
-
-        handler = new MithosHandler(server, topic);
-
-        Properties props = new Properties();
-        props.put("bootstrap.servers", server);
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("linger.ms", 1);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
     }
 
     @Override
@@ -49,17 +39,31 @@ public class MithosExecutor extends ActivityExecutor {
 
     @Override
     public void execute(AbstractActivity activity) {
+        System.out.println("Mithos Kafka action to be executed");
+
         producer.send(new ProducerRecord<String, String>(topic, "key_test", "Message test"));
     }
 
     @Override
     public void launch() {
-        handler.run();
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", server);
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("linger.ms", 1);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        System.out.println("Mithos Kafka producer set up");
+
+        handler = new MithosHandler(server, topic);
+        handler.start();
     }
 
     @Override
     public void unload() {
         producer.close();
-        handler.stopListening();
+        handler.abort();
     }
 }
--- plugins/mithos/src/main/java/de/dfki/vsm/xtension/mithos/MithosHandler.java
+++ plugins/mithos/src/main/java/de/dfki/vsm/xtension/mithos/MithosHandler.java
@@ -1,25 +1,37 @@
 package de.dfki.vsm.xtension.mithos;
 
+import de.dfki.vsm.util.log.LOGConsoleLogger;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.Consumer;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+/**
+ * This plugin uses a kafka server to control an agent-environment and to receive processed userdata.
+ * kafka consumer implementation based on: https://dzone.com/articles/writing-a-kafka-consumer-in-java
+ *
+ * @author Manuel Anglet
+ */
 
 public class MithosHandler extends Thread{
 
     private final String topic;
     private final String server;
-    private final Consumer<Long, String> consumer;
+    private  Consumer<Long, String> consumer;
     private boolean stop = false;
+    private final LOGConsoleLogger mLogger = LOGConsoleLogger.getInstance();
 
     public MithosHandler(String server, String topic) {
         this.server = server;
         this.topic = topic;
+    }
 
+    @Override
+    public synchronized void start() {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.server);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "VSMConsumer");
@@ -29,6 +41,8 @@ public class MithosHandler extends Thread{
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
         consumer = new KafkaConsumer<>(props);
+        System.out.println("Mithos Kafka consumer set up");
+        super.start();
     }
 
     @Override
@@ -36,23 +50,23 @@ public class MithosHandler extends Thread{
 
         Duration duration = Duration.ofMillis(1000);
 
-        while(!stop){
+        System.out.println("Mithos Kafka consumer starts listening");
+
+        while(stop){
             final ConsumerRecords<Long, String> consumerRecords =
                     consumer.poll(duration);
 
             consumerRecords.forEach(record -> {
-                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
-                        record.key(), record.value(),
-                        record.partition(), record.offset());
+                mLogger.message(record.toString());
             });
 
             consumer.commitAsync();
         }
-        consumer.close();
     }
 
-
-    public void stopListening(){
-        this.stop=false;
+    public final void abort() {
+        stop = true;
+        consumer.close();
+        interrupt();
     }
 }
