95b1a91d3b92b840b47b0f85230d0da830cddfc6
Fabrizio Nunnari <fab.nunnari@gmail.com>
2022-09-22 12:15:48 +0200

    Implemented the timed history.

--- plugins/mindbotSSI/src/main/java/de/dfki/vsm/xtension/mindbotssi/MindBotSSIPlugin.java
+++ plugins/mindbotSSI/src/main/java/de/dfki/vsm/xtension/mindbotssi/MindBotSSIPlugin.java
@@ -9,6 +9,8 @@ import de.dfki.vsm.xtension.ssi.event.data.SSIEventData;
 import de.dfki.vsm.xtension.ssi.event.data.SSITupleData;
 
 import java.util.*;
+import java.util.function.DoublePredicate;
+import java.util.stream.Stream;
 
 
 /**
@@ -16,10 +18,23 @@ import java.util.*;
  */
 public class MindBotSSIPlugin extends SSIRunTimePlugin {
 
+    private static final String[] emotionNames = new String[]{"surprise", "happy", "sad", "neutral", "valence", "disgust", "anger", "fear", "arousal"};
+    private static final String[] focusTargets = new String[]{"away", "cobot", "table"} ;
+
+
+    //
+    // Will map a variable to the list of received values. Used to compute the average.
+    private static Map<String, LinkedList<TimedFloat>> timedHistory;
+
+    public static int TIMED_HISTORY_MAX_AGE_MILLIS = 10 * 1000 ;
+
+    public static int TIMED_HISTORY_MIN_SIZE = 30 ;
+
     // Will map a variable to the list of received values. Used to compute the average.
     private static Map<String, List<Float>> history;
-    private final String[] emotionNames = new String[]{"surprise", "happy", "sad", "neutral", "valence", "disgust", "anger", "fear", "arousal"};
-    private final String[] focusTargets = new String[]{"away", "cobot", "table"} ;
+
+
+
 
 
     // Construct SSI plugin
@@ -42,6 +57,12 @@ public class MindBotSSIPlugin extends SSIRunTimePlugin {
         Arrays.stream(focusTargets).forEach(name-> history.put(name,new ArrayList<Float>()));
         history.put("fatigue", new ArrayList<Float>());
         history.put("pain", new ArrayList<Float>());
+
+        timedHistory = new HashMap<>();
+        Arrays.stream(emotionNames).forEach(name-> timedHistory.put(name,new LinkedList<TimedFloat>()));
+        Arrays.stream(focusTargets).forEach(name-> timedHistory.put(name,new LinkedList<TimedFloat>()));
+        timedHistory.put("fatigue", new LinkedList<TimedFloat>());
+        timedHistory.put("pain", new LinkedList<TimedFloat>());
 
     }
 
@@ -90,6 +111,13 @@ public class MindBotSSIPlugin extends SSIRunTimePlugin {
                 assert event_entry.getType().equals("MAP");
                 SSITupleData tupleData = (SSITupleData) data;
                 mLogger.message("Got emotion+valence+arousal values: \t" + tupleData);
+
+                // if there is a face detected
+                boolean there_is_face = Arrays.stream(emotionNames).
+                        mapToDouble(value -> Double.parseDouble(tupleData.get(value)))
+                        .anyMatch(value -> value != 0.0) ;
+
+
                 // For each of the variables expected in this map, compose a corresponding
                 // project variable name prepending the "ssi_emotion_" prefix
                 Arrays.stream(emotionNames).forEach(emotion -> {
@@ -98,10 +126,15 @@ public class MindBotSSIPlugin extends SSIRunTimePlugin {
                     if (mProject.hasVariable(projectVarName)) {
                         mProject.setVariable(projectVarName, ssiVarValue);
                     }
-                    float avgValue = movingAverage(emotion, ssiVarValue);
-                    String projectAvgVarName = "ssi_emotion_" + emotion + "_avg";
-                    if (mProject.hasVariable(projectAvgVarName)) {
-                        mProject.setVariable(projectAvgVarName, avgValue);
+
+                    timedAverageRemoveOldest(emotion);
+
+                    if (there_is_face) {
+                        float avgValue = timedMovingAverage(emotion, ssiVarValue);
+                        String projectAvgVarName = "ssi_emotion_" + emotion + "_avg";
+                        if (mProject.hasVariable(projectAvgVarName)) {
+                            mProject.setVariable(projectAvgVarName, avgValue);
+                        }
                     }
                 });
 
@@ -109,6 +142,11 @@ public class MindBotSSIPlugin extends SSIRunTimePlugin {
                 assert event_entry.getType().equals("MAP");
                 SSITupleData tupleData = (SSITupleData) data;
                 mLogger.message("Got video focus values: \t" + tupleData);
+
+                // if there is a face detected
+                boolean there_is_face = Arrays.stream(focusTargets).
+                        mapToDouble(value -> Double.parseDouble(tupleData.get(value)))
+                        .anyMatch(value -> value != 0.0) ;
 
                 // For each of the variables expected in this map, compose a corresponding
                 // project variable name prepending the "ssi_focus_" prefix
@@ -118,12 +156,18 @@ public class MindBotSSIPlugin extends SSIRunTimePlugin {
                     if (mProject.hasVariable(projectVarName)) {
                         mProject.setVariable(projectVarName, ssiVarValue);
                     }
-                    float avgValue = movingAverage(ssiVarName, ssiVarValue);
-                    String projectAvgVarName = "ssi_focus_" + ssiVarName + "_avg";
-                    if (mProject.hasVariable(projectAvgVarName)) {
-                        mProject.setVariable(projectAvgVarName, avgValue);
+
+                    timedAverageRemoveOldest(ssiVarName);
+
+                    if (there_is_face) {
+                        float avgValue = timedMovingAverage(ssiVarName, ssiVarValue);
+                        String projectAvgVarName = "ssi_focus_" + ssiVarName + "_avg";
+                        if (mProject.hasVariable(projectAvgVarName)) {
+                            mProject.setVariable(projectAvgVarName, avgValue);
+                        }
                     }
                 }
+
             } else if (sender.equals("biomech") && event.equals("fatigue")) {
                 assert event_entry.getType().equals("MAP");
                 SSITupleData tupleData = (SSITupleData) data;
@@ -149,28 +193,132 @@ public class MindBotSSIPlugin extends SSIRunTimePlugin {
                  mProject.setVariable(projectAvgVarName, avgValue);
                  }
                  */
+
             } else if (sender.equals("video") && event.equals("pain")) {
                 assert event_entry.getType().equals("MAP");
                 SSITupleData tupleData = (SSITupleData) data;
                 mLogger.message("Got pain data: \t" + tupleData);
 
                 float painValue = Float.parseFloat(tupleData.get("pain"));
+                float noPainValue = Float.parseFloat(tupleData.get("no pain")) ;
+                boolean there_is_face = painValue != 0.0f || noPainValue != 0.0f ;
+
                 if (mProject.hasVariable("ssi_pain")) {
                     mProject.setVariable("ssi_pain", painValue);
                 }
 
-                float avgPainValue = movingAverage("pain", painValue);
-                if (mProject.hasVariable("ssi_pain_avg")) {
-                    mProject.setVariable("ssi_pain_avg", avgPainValue);
+                timedAverageRemoveOldest("pain");
+
+                // if there is a face detected
+                if (there_is_face) {
+                    // float avgPainValue = movingAverage("pain", painValue);
+                    float avgPainValue = timedMovingAverage("pain", painValue) ;
+                    if (mProject.hasVariable("ssi_pain_avg")) {
+                        mProject.setVariable("ssi_pain_avg", avgPainValue);
+                    }
                 }
 
-                // float noPainValue = Float.parseFloat(tupleData.get("no pain")) ;
-                // Handle here, if needed
             }
 
         } // end for(...)
 
     } // end handle()
+
+
+    private static void timedAverageRemoveOldest(String name) {
+        timedAverageRemoveOldest(timedHistory.get(name));
+    }
+
+    private static void timedAverageRemoveOldest(LinkedList<TimedFloat> history) {
+        timedAverageRemoveOldest(history, System.currentTimeMillis());
+    }
+
+    private static synchronized void timedAverageRemoveOldest(LinkedList<TimedFloat> history, long now) {
+
+        // Remove all "old" elements
+        while(history.size() > 0) {
+            TimedFloat f = history.getFirst();
+            // If the first element is too old, remove it
+            if(now - f.t > TIMED_HISTORY_MAX_AGE_MILLIS) {
+                history.removeFirst() ;
+            } else {
+                // if it is young enough, we can stop here.
+                break ;
+            }
+        }
+
+    }
+
+    /** Adds a value to the history of the specified category, and computes the average.
+     * Additionally, each added value is associated to a time stamp. After adding a new value, all values
+     * older than a given "age" will be removed before computing the average.
+     *
+     * @param name
+     * @param value
+     * @return
+     */
+    private synchronized float timedMovingAverage(String name, float value){
+
+        LinkedList<TimedFloat> history = MindBotSSIPlugin.timedHistory.get(name);
+        long now = System.currentTimeMillis();
+
+        // add the new element
+        history.addLast(new TimedFloat(now, value));
+        // and compute the average
+        return (float) history.stream()
+                .mapToDouble(tf -> (double) tf.v)
+                .average().orElse(0.0);
+    }
+
+    private static class TimedFloat {
+        /* Value */
+        float v;
+        /* Timestamp in milliseconds */
+        long t;
+        TimedFloat(long t, float v) {
+            this.t = t ;
+            this.v = v ;
+        }
+        TimedFloat(float v) {
+            this(System.currentTimeMillis(), v) ;
+        }
+    }
+
+
+    public static int getTimedHistoryMaxAgeMillis() {
+        return TIMED_HISTORY_MAX_AGE_MILLIS;
+    }
+
+    public static void setTimedHistoryMaxAgeMillis(int timedHistoryMaxAgeMillis) {
+        TIMED_HISTORY_MAX_AGE_MILLIS = timedHistoryMaxAgeMillis;
+    }
+
+    public static int getTimedHistoryMinSize() {
+        return TIMED_HISTORY_MIN_SIZE;
+    }
+
+    public static void setTimedHistoryMinSize(int timedHistoryMinSize) {
+        TIMED_HISTORY_MIN_SIZE = timedHistoryMinSize;
+    }
+
+    public static synchronized boolean isTimedHistoryValid(String name) {
+        LinkedList<TimedFloat> h = timedHistory.get(name);
+
+        long now = System.currentTimeMillis() ;
+        timedAverageRemoveOldest(h, now);
+        assert h.size() == 0 || (now - h.getFirst().t) <= TIMED_HISTORY_MAX_AGE_MILLIS;
+
+        return h.size() >= TIMED_HISTORY_MIN_SIZE ;
+    }
+
+    public static boolean isTimedHistoryValid() {
+        return isTimedHistoryValid("pain") ;
+    }
+
+    public static synchronized void resetTimedHistory() {
+        timedHistory.forEach((s, l)->l.clear());
+    }
+
 
     private float movingAverage(String name, Float value){
         List<Float> history = MindBotSSIPlugin.history.get(name);
@@ -189,7 +337,7 @@ public class MindBotSSIPlugin extends SSIRunTimePlugin {
     //
 
     public static float varStep = 0.2f ;
-    public static float varMin = 0.0f;
+    public static float varMin = 0.2f;
     public static float varMax = 1.0f;
 
     public static float decreaseVar(float v) {
