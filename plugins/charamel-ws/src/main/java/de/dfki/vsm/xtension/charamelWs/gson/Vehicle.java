4d39733e0cebdaa925e68451565e2a77d67d5160
Manuel <manuel.anglet@dfki.de>
2024-02-01 17:00:06 +0100

    added build.gradle

---
+++ plugins/charamel-ws/src/main/java/de/dfki/vsm/xtension/charamelWs/gson/Vehicle.java
@@ -0,0 +1,14 @@
+package de.dfki.vsm.xtension.charamelWs.gson;
+
+public class Vehicle {
+    private int id;
+    private double distance;
+    private double distToRef;
+    private double roadS;
+    private int laneId;
+    private double speed;
+    private double length;
+    private double width;
+    private double height;
+
+}
