7e528951e73068dc35ab998e833c054e648c21c1
Manuel <manuel.anglet@dfki.de>
2024-02-05 01:10:51 +0100

    Created a Wrapper for sockets in VMS plugins

---
+++ plugins/sockets/src/main/java/de/dfki/vsm/xtenstion/sockets/AbsJavaSocket.java
@@ -0,0 +1,71 @@
+package de.dfki.vsm.xtenstion.sockets;
+
+import de.dfki.vsm.util.log.LOGConsoleLogger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+
+/**
+ * @author Manuel Anglet
+ */
+abstract class AbsJavaSocket extends Thread {
+
+    // The logger instance
+    final LOGConsoleLogger logger = LOGConsoleLogger.getInstance();
+    // The executor instance
+    final VSMSocketHandler executor;
+    final String host;
+
+    int port;
+
+    // The socket streams
+    OutputStreamWriter outWriter;
+    BufferedReader inReader;
+    // The termination flag
+    boolean done = false;
+
+
+    // Create the client thread
+    public AbsJavaSocket(VSMSocketHandler executor, int port) {
+        super();
+        // Initialize the socket
+        this.executor = executor;
+        this.port = port;
+        this.host = "localhost";
+    }
+
+    public AbsJavaSocket(VSMSocketHandler executor, String host, int port) {
+        super();
+        // Initialize the socket
+        this.executor = executor;
+        this.port = port;
+        this.host = host;
+    }
+
+    // Start the client thread
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    // Abort the client thread
+    abstract public  void abort() ;
+
+    abstract public  void connect() ;
+
+    // Send some message
+    public boolean send(final String message) {
+        try {
+            outWriter.write(message);
+            return true;
+        } catch (IOException e) {
+            logger.failure(e.toString());
+            return false;
+        }
+    }
+
+
+
+}
